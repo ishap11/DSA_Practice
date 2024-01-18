6bd06444c4c6dc3d379930fc981d005f8ab565c7
Isha Prajapati <128213933+ishap11@users.noreply.github.com>
2024-01-18 16:35:17 +0530

    Time: 1 ms (99.93%), Space: 57.5 MB (52.71%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        // BRUTE FORCE  T(c)- O(n^3) Sc - O(1)
+        // int ans = Integer.MIN_VALUE;
+        // for(int i=0; i< nums.length ;i++){
+        //     for(int j= i ; j< nums.length ; j++){
+        //         int sum =0;
+        //         for(int k= i ; k< j; k++){
+        //             sum += nums[k];
+        //         }
+        //         ans = Math.max(sum , ans);
+        //     }
+        // }
+        // return ans;
+
+//         Optimised Approach TC - O(N) SC - O(1)
+        int sum =0;
+        int ans = Integer.MIN_VALUE;
+
+        for(int i=0; i< nums.length  ;i++){
+            sum = sum + nums[i]; //calculate sum
+
+            ans = Math.max(ans ,sum); 
+            //max sum me se ans me se kya h vo stre krrhe h 
+            if(sum <0){  //check agr element -ve h toh consider hi ni krna
+                sum=0;
+            }
+        }
+        return ans;
+    }
+}
