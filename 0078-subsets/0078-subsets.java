c136f90a73c6395d0c17b7001a5baa6992077353
Isha Prajapati <128213933+ishap11@users.noreply.github.com>
2024-09-07 21:01:06 +0530

    Time: 0 ms (100%), Space: 42.7 MB (52.63%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -2,19 +2,25 @@ class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         List<Integer> subset = new ArrayList<>();
-
-        createsubsets(nums, 0, res, subset);
-        return res; 
+        createSubsets(nums, nums.length - 1, res, subset);
+        return res;
     }
-    public void createsubsets(int[] nums , int index , List<List<Integer>> res , List<Integer> subset) {
-        if(index == nums.length){
+
+    public void createSubsets(int[] nums, int index, List<List<Integer>> res, List<Integer> subset) {
+        // Base case: if index is less than 0, add the current subset to result and return
+        if (index < 0) {
             res.add(new ArrayList<>(subset));
             return;
         }
-        subset.add(nums[index]);
-        createsubsets(nums , index +1 , res , subset);
 
+        // Exclude the current element and move to the next element (towards the start)
+        createSubsets(nums, index - 1, res, subset);
+
+        // Include the current element in the subset and move to the next element (towards the start)
+        subset.add(nums[index]);
+        createSubsets(nums, index - 1, res, subset);
+
+        // Backtrack by removing the last element added
         subset.remove(subset.size() - 1);
-        createsubsets(nums, index + 1, res, subset);
     }
 }
