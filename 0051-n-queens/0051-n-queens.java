2a43837a20520c57b269962d44328c15ba405762
Isha Prajapati <128213933+ishap11@users.noreply.github.com>
2024-09-13 01:04:26 +0530

    Time: 2 ms (89.48%), Space: 44.7 MB (79.64%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.java
@@ -0,0 +1,51 @@
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> result = new ArrayList<>();
+
+        boolean[][] board = new boolean[n][n];
+        boolean[] cols = new boolean[n];
+        boolean[] ndiag = new boolean[2*n-1];
+        boolean[] rdiag = new boolean[2*n-1];
+
+        solve(board , 0 , cols , ndiag , rdiag , result);
+        return result;
+    }
+
+    public void solve( boolean[][] board , int row ,  boolean[] cols  , boolean[] ndiag , boolean[] rdiag , List<List<String>> result) {
+        
+        if (row == board.length) {
+            // Base case: If all rows are filled, add the current board configuration to the result
+            List<String> solution = new ArrayList<>();
+            for (int i = 0; i < board.length; i++) {
+                StringBuilder sb = new StringBuilder();
+                for (int j = 0; j < board[0].length; j++) {
+                    if (board[i][j]) {
+                        sb.append("Q");
+                    } else {
+                        sb.append(".");
+                    }
+                }
+                solution.add(sb.toString());
+            }
+            result.add(solution);
+            return;
+        }
+
+        for(int col =0 ; col < board[0].length ; col++){
+            if(cols[col] == false && ndiag[row + col] == false && rdiag[row - col + board.length - 1] ==false){
+                board[row][col] = true;
+                cols[col] = true;
+                ndiag[row+col] = true;
+                rdiag[row - col + board.length - 1]  = true;
+
+                solve(board, row + 1, cols, ndiag, rdiag, result);
+
+                board[row][col] = false;
+                cols[col] = false;
+                ndiag[row+col] = false;
+                rdiag[row - col + board.length - 1]  = false;
+
+            }
+        }
+    }
+}
