62c57f7e1c306ced3c069a8107a7286469bd8f1d
Isha Prajapati <128213933+ishap11@users.noreply.github.com>
2024-09-16 22:28:02 +0530

    Time: 0 ms (100%), Space: 40.2 MB (66.46%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int totalNQueens(int n) {
+        int[] count = {0}; 
+
+        boolean[][] board = new boolean[n][n];
+        boolean[] cols = new boolean[n];
+        boolean[] ndiag = new boolean[2*n-1];
+        boolean[] rdiag = new boolean[2*n-1];
+
+        solve(board , 0 , cols , ndiag , rdiag , count);
+        return count[0];
+    }
+    public void solve( boolean[][] board , int row ,  boolean[] cols  , boolean[] ndiag , boolean[] rdiag , int[] count) {
+        
+        if (row == board.length) {
+            // Base case: If all rows are filled, add the current board configuration to the result
+            count[0]++;
+            return;
+
+        }
+
+        for(int col =0 ; col < board[0].length ; col++){
+            if(cols[col] == false && ndiag[row + col] == false && rdiag[row - col + board.length - 1] ==false){
+                board[row][col] = true;
+                cols[col] = true;
+                ndiag[row+col] = true;
+                rdiag[row - col + board.length - 1]  = true;
+
+                solve(board, row + 1, cols, ndiag, rdiag, count);
+
+                board[row][col] = false;
+                cols[col] = false;
+                ndiag[row+col] = false;
+                rdiag[row - col + board.length - 1]  = false;
+
+            }
+        }
+    }
+}
