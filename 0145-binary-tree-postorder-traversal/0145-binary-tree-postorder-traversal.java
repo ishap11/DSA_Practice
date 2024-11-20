a4dc638f96a4e28a54524cb7f16ebf6417f62e88
Isha Prajapati <128213933+ishap11@users.noreply.github.com>
2024-11-20 20:11:15 +0530

    Time: 0 ms (100%), Space: 41.5 MB (67.4%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -22,27 +22,56 @@ class Solution {
 
 
         // usng 2 stacks
+        // List<Integer> ans = new ArrayList<Integer>();
+        // Stack<TreeNode> st1 = new Stack<TreeNode>();
+        // Stack<TreeNode> st2 =  new Stack<TreeNode>();
+
+        // if(root == null){
+        //     return ans;
+        // }
+        // st1.push(root);
+        // while(!st1.isEmpty()){
+        //     root=  st1.pop();
+        //     st2.add(root);
+
+        //     if(root.left != null) {
+        //         st1.push(root.left);
+        //     }
+        //     if(root.right != null){
+        //         st1.push(root.right);
+        //     }
+        // }
+        // while(!st2.isEmpty()){
+        //     ans.add(st2.pop().val);
+        // }
+        // return ans;
+
+
+
+        // using 1 stack
         List<Integer> ans = new ArrayList<Integer>();
-        Stack<TreeNode> st1 = new Stack<TreeNode>();
-        Stack<TreeNode> st2 =  new Stack<TreeNode>();
+        Stack<TreeNode> st = new Stack<TreeNode>();
 
-        if(root == null){
-            return ans;
-        }
-        st1.push(root);
-        while(!st1.isEmpty()){
-            root=  st1.pop();
-            st2.add(root);
+        TreeNode curr = root;
 
-            if(root.left != null) {
-                st1.push(root.left);
+        while(curr!= null  || !st.isEmpty()){
+            if(curr != null){
+                st.push(curr);
+                curr = curr.left;
+            }else{
+                TreeNode temp =  st.peek().right;
+                if(temp == null){
+                    temp = st.pop();
+                    ans.add(temp.val);
+
+                    while(!st.isEmpty()  && temp == st.peek().right){
+                        temp = st.pop();
+                        ans.add(temp.val);
+                    }
+                }else{
+                    curr = temp;
+                }
             }
-            if(root.right != null){
-                st1.push(root.right);
-            }
-        }
-        while(!st2.isEmpty()){
-            ans.add(st2.pop().val);
         }
         return ans;
     }
