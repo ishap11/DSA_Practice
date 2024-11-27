3949f2319c858e3c9a6baafb7392053ebd00d1fa
Isha Prajapati <128213933+ishap11@users.noreply.github.com>
2024-11-27 22:44:07 +0530

    Time: 2 ms (22.48%), Space: 45 MB (8.83%) - LeetHub

---
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
@@ -0,0 +1,63 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public TreeNode getRightMostTree(TreeNode node , TreeNode curr){
+        while(node.right != null && node.right != curr){
+            node = node.right;
+        }
+        return node;
+    }
+    public void recoverTree(TreeNode root) {
+        TreeNode prev = null , curr = root , a = null , b = null;
+
+        while(curr != null){
+            TreeNode leftNode = curr.left;
+            if(leftNode == null){
+                if(prev != null && prev.val > curr.val){
+                    if(a == null){
+                        a = prev;
+                    }
+                    b = curr;
+                }
+                prev = curr;
+                curr = curr.right;
+            }else {
+                TreeNode rmn = getRightMostTree(leftNode, curr);
+
+                if(rmn.right == null) {
+                    rmn.right = curr;
+                    curr = curr.left;
+                }else {
+                    rmn.right = null;
+                    if(prev.val > curr.val){
+                        if(a == null){
+                            a = prev;
+                        }
+                        b = curr;
+                    }
+                    prev = curr;
+                    curr = curr.right;
+                }
+            }
+        }
+        if(a != null){
+            int temp = a.val;
+            a.val = b.val;
+            b.val = temp;
+        }
+    }
+}
