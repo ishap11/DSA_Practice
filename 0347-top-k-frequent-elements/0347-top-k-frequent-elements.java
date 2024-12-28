b4b1de01ff985db073a3f00a652286feccf3d454
Isha Prajapati <128213933+ishap11@users.noreply.github.com>
2024-12-28 21:17:08 +0530

    Time: 11 ms (91.69%), Space: 48.6 MB (58.87%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,31 +1,61 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
+        // TC  - O(k⋅n) SC - O(m+k)
+        // HashMap<Integer , Integer> map = new HashMap<>();
+        // int[] ans = new int[k];
+        // for(int num : nums){
+        //     map.put(num , map.getOrDefault(num , 0) +1);
+        // }
+        
+        //  // Step 2: Find the top k frequent elements
+        // for (int i = 0; i < k; i++) {
+        //     int maxFreq = 0;
+        //     int maxKey = 0;
+            
+        //     // Iterate over the map to find the element with the highest frequency
+        //     for (int key : map.keySet()) {
+        //         if (map.get(key) > maxFreq) {
+        //             maxFreq = map.get(key);
+        //             maxKey = key;
+        //         }
+        //     }
+            
+        //     // Add the most frequent element to the result array
+        //     ans[i] = maxKey;
+            
+        //     // Remove the most frequent element from the map
+        //     map.remove(maxKey);
+        // }
+        
+        // return ans;
+
+
+        // TC - O(n) Sc- O(1)
+        List<Integer> [] bucket = new List[nums.length + 1];
         HashMap<Integer , Integer> map = new HashMap<>();
-        int[] ans = new int[k];
+
         for(int num : nums){
             map.put(num , map.getOrDefault(num , 0) +1);
         }
-        
-         // Step 2: Find the top k frequent elements
-        for (int i = 0; i < k; i++) {
-            int maxFreq = 0;
-            int maxKey = 0;
-            
-            // Iterate over the map to find the element with the highest frequency
-            for (int key : map.keySet()) {
-                if (map.get(key) > maxFreq) {
-                    maxFreq = map.get(key);
-                    maxKey = key;
+
+        for(int key : map.keySet()) {
+            int freq = map.get(key);
+            if(bucket[freq] == null ) {
+                bucket[freq] = new ArrayList<>();
+            }
+            bucket[freq].add(key);
+        }
+
+        int[] res = new int[k];
+        int counter = 0;
+
+        for(int pos = bucket.length -1; pos >=0 && counter < k ; pos--) {
+            if(bucket[pos] != null) {
+                for(Integer integer : bucket[pos]) {
+                    res[counter++] = integer;
                 }
             }
-            
-            // Add the most frequent element to the result array
-            ans[i] = maxKey;
-            
-            // Remove the most frequent element from the map
-            map.remove(maxKey);
         }
-        
-        return ans;
+        return res;
     }
 }
