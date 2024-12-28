3e0f9b377d9bc1c1c611e9774ddf0ca512e24508
Isha Prajapati <128213933+ishap11@users.noreply.github.com>
2024-12-28 23:35:35 +0530

    Time: 0 ms (100%), Space: 44.5 MB (49.29%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -15,40 +15,43 @@
  */
 class Solution {
     public int kthSmallest(TreeNode root, int k) {
-        // Stack<TreeNode> st = new Stack<>();
+        // TC  - O(k) SC - O(h)
+        Stack<TreeNode> st = new Stack<>();
 
-        // while (root != null || !st.isEmpty()) {
-        //     while (root != null) {
-        //         st.push(root);
-        //         root = root.left;
-        //     }
+        while (root != null || !st.isEmpty()) {
+            while (root != null) {
+                st.push(root);
+                root = root.left;
+            }
             
-        //     root = st.pop();
+            root = st.pop();
             
-        //     // Decrement k, as we've visited one more node
-        //     k--;
+            // Decrement k, as we've visited one more node
+            k--;
             
-        //     // If k is 0, we've found the k-th smallest element
-        //     if (k == 0) {
-        //         return root.val;
-        //     }
-        //     root = root.right;
-        // }
-        //  throw new IllegalArgumentException("k is larger than the number of nodes in the tree");
-
-        List<Integer> ans = new ArrayList<>();
-        inorder(root , ans);
-
-        return ans.get(k - 1);
-
-    }
-
-    public void inorder(TreeNode root , List<Integer> ans ){
-        if(root == null){
-            return ;
+            // If k is 0, we've found the k-th smallest element
+            if (k == 0) {
+                return root.val;
+            }
+            root = root.right;
         }
-        inorder(root.left , ans);
-        ans.add(root.val);
-        inorder(root.right , ans);
+         throw new IllegalArgumentException("k is larger than the number of nodes in the tree");
+
+
+        // TC- O(n) Sc - O(n)
+        // List<Integer> ans = new ArrayList<>();
+        // inorder(root , ans);
+
+        // return ans.get(k - 1);
+
     }
+
+    // public void inorder(TreeNode root , List<Integer> ans ){
+    //     if(root == null){
+    //         return ;
+    //     }
+    //     inorder(root.left , ans);
+    //     ans.add(root.val);
+    //     inorder(root.right , ans);
+    // }
 }
