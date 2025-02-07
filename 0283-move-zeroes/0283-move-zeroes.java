60952fda732e59e01d03a9c0d9ac3ee539a2adcd
Isha Prajapati <128213933+ishap11@users.noreply.github.com>
2025-02-07 18:30:48 +0530

    Time: 2 ms (84.78%), Space: 45.9 MB (63.48%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,17 +1,33 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int[] ans = new int[nums.length];
-        int j= 0;
-        for(int i=0; i< nums.length ; i++) {
-            if(nums[i] != 0) {
-                ans[j++] = nums[i];
+
+        // TC- O(n) SC - O(n)
+        // int[] ans = new int[nums.length];
+        // int j= 0;
+        // for(int i=0; i< nums.length ; i++) {
+        //     if(nums[i] != 0) {
+        //         ans[j++] = nums[i];
+        //     }
+        // }
+        // while (j < nums.length ){
+        //     ans[j++] = 0;
+        // }
+        // for (int i = 0; i < nums.length ; i++) {
+        //     nums[i] = ans[i];
+        // }
+
+        int i = 0;
+        int j = 0;
+        int temp;
+
+        while (i < nums.length) {
+            if (nums[i] != 0) {
+                temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
             }
-        }
-        while (j < nums.length ){
-            ans[j++] = 0;
-        }
-        for (int i = 0; i < nums.length ; i++) {
-            nums[i] = ans[i];
+            i++;
         }
     }
 }
