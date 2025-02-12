112abc41c6204e594b5042d9f82db997110a84e2
Isha Prajapati <128213933+ishap11@users.noreply.github.com>
2025-02-12 13:33:30 +0530

    Time: 8 ms (100%), Space: 45.7 MB (39.45%) - LeetHub

--- 2413-smallest-number-in-infinite-set/2413-smallest-number-in-infinite-set.java
+++ 2413-smallest-number-in-infinite-set/2413-smallest-number-in-infinite-set.java
@@ -1,27 +1,55 @@
+// class SmallestInfiniteSet {
+
+//     private TreeSet<Integer> set;
+//     private int nextNum;
+
+//     public SmallestInfiniteSet() {
+//         set = new TreeSet<>();
+//         nextNum = 1;
+//     }
+    
+//     public int popSmallest() {
+//         if(!set.isEmpty()) {
+//            return set.pollFirst();
+//         }
+//         return nextNum++;
+//     }
+    
+//     public void addBack(int num) {
+//         if(num < nextNum && !set.contains(num)) {
+//             set.add(num);
+//         } 
+//     }
+// }
+
+
 class SmallestInfiniteSet {
-
-    private TreeSet<Integer> set;
-    private int nextNum;
-
+    int i=1;
+    public static PriorityQueue<Integer> pq;
     public SmallestInfiniteSet() {
-        set = new TreeSet<>();
-        nextNum = 1;
+        pq = new PriorityQueue<Integer>();
     }
     
     public int popSmallest() {
-        if(!set.isEmpty()) {
-           return set.pollFirst();
+        if(pq.size()==0){
+            return i++;
         }
-        return nextNum++;
+        return pq.poll();
     }
     
     public void addBack(int num) {
-        if(num < nextNum && !set.contains(num)) {
-            set.add(num);
-        } 
+        if(num<i && !pq.contains(num)){
+            pq.add(num);
+        }
     }
 }
 
+/**
+ * Your SmallestInfiniteSet object will be instantiated and called as such:
+ * SmallestInfiniteSet obj = new SmallestInfiniteSet();
+ * int param_1 = obj.popSmallest();
+ * obj.addBack(num);
+ */
 /**
  * Your SmallestInfiniteSet object will be instantiated and called as such:
  * SmallestInfiniteSet obj = new SmallestInfiniteSet();
