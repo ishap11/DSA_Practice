c2b0d915ee33f83b055a6799d01a48d4b219d762
Isha Prajapati <ishaprajapati112004@gmail.com>
2025-03-21 23:42:43 +0530

    Time: 8 ms (9.93%), Space: 43.5 MB (77.91%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> ans=new ArrayList<>();
+        Arrays.sort(nums);
+        generateSubsets(0,ans,nums,new ArrayList<>());
+        return ans;
+    }
+
+     public void generateSubsets(int index,List<List<Integer>> ans,int[] nums,List<Integer> list){
+        if(index == nums.length){
+            if(!ans.contains(list)){
+                ans.add(new ArrayList<>(list));
+            }
+            return;
+        }
+        list.add(nums[index]);
+        generateSubsets(index+1,ans,nums,list);
+        list.remove(list.size()-1);
+        generateSubsets(index+1,ans,nums,list);
+    }
+}
