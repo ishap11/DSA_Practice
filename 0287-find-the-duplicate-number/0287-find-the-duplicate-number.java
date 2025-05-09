7e338922d784edd27b862dcd9b255ac712a5b151
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-05-10 00:03:40 +0530

    Time: 23 ms (25.7%), Space: 58.9 MB (11.87%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,15 +1,39 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
+        // HashSet<Integer> set = new HashSet<>();
 
-        for(int num : nums ){
-            if(!set.contains(num)){
-                set.add(num);
+        // for(int num : nums ){
+        //     if(!set.contains(num)){
+        //         set.add(num);
+        //     }
+        //     else if (set.contains(num)){
+        //         return num;
+        //     }
+        // }
+        // return -1;
+
+        int left = 1;
+        int right = nums.length - 1; // since nums.length == n + 1
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+
+            // Count how many numbers are <= mid
+            int count = 0;
+            for (int num : nums) {
+                if (num <= mid) {
+                    count++;
+                }
             }
-            else if (set.contains(num)){
-                return num;
+
+            // Binary search decision
+            if (count > mid) {
+                right = mid; // duplicate is in the left half
+            } else {
+                left = mid + 1; // duplicate is in the right half
             }
         }
-        return -1;
+
+        return left; // or right, since left == right
     }
 }
