74435c3faa71f341ab2b47ca862eeddb3f161ce6
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-05-09 20:02:14 +0530

    Time: 0 ms (100%), Space: 45.5 MB (99.43%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,42 +1,54 @@
 class Solution {
-    public int first(int[] nums , int target){
-        int lo = 0;
-        int hi = nums.length - 1;
-        int ans =-1;
-
-        while(lo <= hi){
-            int mid = (lo + hi)/2;
-            if(nums[mid] == target){
-                ans = mid;
-                hi = mid -1;
-            }else if(nums[mid] < target){
-                lo = mid +1;
-            }else{
-                hi = mid -1;
-            }
-        }
-        return ans;
-    }
-    public int last(int[] nums , int target){
-        int lo = 0;
-        int hi = nums.length - 1;
-        int ans =-1;
-
-        while(lo <= hi){
-            int mid = (lo + hi)/2;
-            if(nums[mid] == target){
-                ans = mid;
-                lo = mid + 1;
-            }else if(nums[mid] < target){
-                lo = mid +1;
-            }else{
-                hi = mid -1;
-            }
-        }
-        return ans;
-    }
-
     public int[] searchRange(int[] nums, int target) {
-        return new int[]{first(nums, target), last(nums, target)};
+
+       int firstIdx =  firstIndex(nums , target) ;
+       int lastIdx = lastIndex(nums , target);
+
+        if (firstIdx == -1 || lastIdx == -1) {
+            return new int[]{-1, -1};
+        }
+        
+       return new int[]{firstIdx  , lastIdx};
+        
+    }
+
+    public int firstIndex(int[] nums , int target) {
+        int low = 0 ;
+        int high =  nums.length -1;
+        int result = -1;
+
+        while(low <= high) {
+            int mid = (low + high)/2;
+
+            if(nums[mid] < target) {
+                low = mid + 1;
+            } else if(nums[mid] > target){
+                high = mid -1;
+            } else {
+                result = mid;
+                high = mid -1;
+            }
+        }
+        return result;
+    }
+
+    public int  lastIndex(int[] nums , int target) {
+        int low = 0 ;
+        int high =  nums.length -1;
+        int result = -1;
+
+        while(low <= high) {
+            int mid = (low + high)/2;
+
+            if(nums[mid] < target) {
+                low = mid + 1;
+            } else if(nums[mid] > target){
+                high = mid -1;
+            } else {
+                result = mid;
+                low = mid + 1;
+            }
+        }
+        return result;
     }
 }
