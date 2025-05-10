cef6e1e06bf1f66ffab0df75af0e728c0bc3f72b
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-05-10 22:41:59 +0530

    Time: 29 ms (77.08%), Space: 67.9 MB (21.35%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,23 +1,89 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        if(nums.length == 0) {
-            return 0;
-        }
+
+        // TLE
+        // HashSet<Integer> set = new HashSet<>();
+
+        // for(int num : nums) {
+        //     set.add(num);
+        // }
+
+        // int longSeq = 0;
+
+        // for(int num : nums) {
+        //       // Step 2: check if it's the start of a sequence
+        //     if(!set.contains(num - 1)) {
+        //         int currNum = num;
+        //         int currStreak = 1;
+
+        //         // Step 3: count the sequence length
+        //         while (set.contains(currNum + 1)) {
+        //             currNum += 1;
+        //             currStreak += 1;
+        //         }
+        //         // Step 4: update longest
+        //         longSeq = Math.max(longSeq, currStreak);
+        //     }
+            
+        // }
+        // return longSeq;
+
+
+
+        /*==========================CODE=================================== */
+
+        // HashMap<Integer , Boolean> map = new HashMap<>();
+
+        // for(int num : nums) {
+        //     map.put(num , true);
+        // }
+
+        // for(int num : nums) {
+        //     if(map.containsKey(num - 1)) {
+        //         map.put(num , false);
+        //     }
+        // }
+
+        // int maxStartPoint = 0 ;
+        // int maxLength = 0;
+
+        // for(int num : nums) {
+        //     if(map.get(num) == true) {
+        //         int tempLength = 1;
+        //         int tempStartPoint = num;
+
+        //         while(map.containsKey(tempStartPoint +  tempLength)) {
+        //             tempLength++;
+        //         }
+
+        //         if(tempLength > maxLength) {
+        //             maxLength = tempLength;
+        //         }
+        //     }
+        // }
+        // return maxLength;
         
-        Arrays.sort(nums);
-        int count = 1;
-        int max_count = 1;
-        
-        for(int i = 0; i < nums.length - 1; i++) {
-            if(nums[i + 1] - nums[i] == 1) {
-                count++;
-            } else if(nums[i + 1] - nums[i] == 0) {
-                continue; 
-            } else {
-                max_count = Math.max(max_count, count);
-                count = 1; 
+
+
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) set.add(num);
+
+        int maxLength = 0;
+
+        for (int num : set) {  
+            if (!set.contains(num - 1)) { 
+                int curNum = num;
+                int currLen = 1;
+
+                while (set.contains(curNum + 1)) {
+                    curNum++;
+                    currLen++;
+                }
+
+                maxLength = Math.max(maxLength, currLen);
             }
         }
-        return Math.max(max_count, count);
+
+        return maxLength;
     }
 }
