68f053e9b0706e3c3cc7d7315bafb9955dcf4a24
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-05-14 21:58:36 +0530

    Time: 4 ms (36.98%), Space: 44.7 MB (7.46%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,26 +1,53 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int[] res = new int[nums1.length] ;
 
-        for (int i = 0; i < nums2.length; i++) {
-        int nextGreater = -1;
+        // BRUTE FORCE
+    //     HashMap<Integer, Integer> map = new HashMap<>();
+    //     int[] res = new int[nums1.length] ;
 
-        for (int j = i + 1; j < nums2.length; j++) {
-            if (nums2[j] > nums2[i]) {
-                nextGreater = nums2[j];
-                break; 
-            }
-        }
+    //     for (int i = 0; i < nums2.length; i++) {
+    //     int nextGreater = -1;
 
-        map.put(nums2[i], nextGreater); 
-    }
+    //     for (int j = i + 1; j < nums2.length; j++) {
+    //         if (nums2[j] > nums2[i]) {
+    //             nextGreater = nums2[j];
+    //             break; 
+    //         }
+    //     }
+
+    //     map.put(nums2[i], nextGreater); 
+    //    }
  
 
-        for(int i=0 ; i< nums1.length ; i++) {
-            res[i] = map.getOrDefault(nums1[i], -1);
+    //     for(int i=0 ; i< nums1.length ; i++) {
+    //         res[i] = map.getOrDefault(nums1[i], -1);
+    //     }
+
+    //     return res;
+
+        HashMap<Integer, Integer> map = new HashMap<>(); 
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = nums2.length - 1; i >= 0; i--) {
+            int current = nums2[i];
+
+        
+            while (!stack.isEmpty() && stack.peek() <= current) {
+                stack.pop();
+            }
+
+            int nextGreater = stack.isEmpty() ? -1 : stack.peek();
+            map.put(current, nextGreater);
+
+            stack.push(current);
         }
 
-        return res;
+        int[] result = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            result[i] = map.getOrDefault(nums1[i], -1);
+        }
+
+        return result;
+    
     }
 }
