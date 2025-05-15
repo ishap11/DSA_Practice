88f23d0af3b9d147bd960a230c6d3abe65a8af7d
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-05-15 19:51:31 +0530

    Time: 2 ms (88.61%), Space: 45.3 MB (9.38%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,18 +1,20 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
-        List<List<String>> result = new ArrayList<>();
+        List<List<String>> res = new ArrayList<>();
 
         boolean[][] board = new boolean[n][n];
         boolean[] cols = new boolean[n];
         boolean[] ndiag = new boolean[2*n-1];
         boolean[] rdiag = new boolean[2*n-1];
 
-        solve(board , 0 , cols , ndiag , rdiag , result);
-        return result;
+        solve(board , 0 , cols , ndiag , rdiag ,  res);
+        return res;
     }
 
-    public void solve( boolean[][] board , int row ,  boolean[] cols  , boolean[] ndiag , boolean[] rdiag , List<List<String>> result) {
-        
+
+    public void solve( boolean[][] board , int row ,  boolean[] cols  , boolean[] ndiag , boolean[] rdiag , List<List<String>> res) {
+
+
         if (row == board.length) {
             // Base case: If all rows are filled, add the current board configuration to the result
             List<String> solution = new ArrayList<>();
@@ -27,25 +29,25 @@ class Solution {
                 }
                 solution.add(sb.toString());
             }
-            result.add(solution);
+            res.add(solution);
             return;
         }
 
-        for(int col =0 ; col < board[0].length ; col++){
-            if(cols[col] == false && ndiag[row + col] == false && rdiag[row - col + board.length - 1] ==false){
+        for(int col = 0;  col < board[0].length ; col++) {
+            if(cols[col] == false && ndiag[row + col ]  == false && rdiag[row - col + board.length -1] == false) {
                 board[row][col] = true;
                 cols[col] = true;
-                ndiag[row+col] = true;
-                rdiag[row - col + board.length - 1]  = true;
+                ndiag[row + col ] = true;
+                rdiag[row - col + board.length -1] = true;
 
-                solve(board, row + 1, cols, ndiag, rdiag, result);
+                solve(board, row + 1, cols, ndiag, rdiag, res);
 
                 board[row][col] = false;
                 cols[col] = false;
-                ndiag[row+col] = false;
-                rdiag[row - col + board.length - 1]  = false;
+                ndiag[row + col ] = false;
+                rdiag[row - col + board.length -1] = false;
 
-            }
+            }  
         }
-    }
+    } 
 }
