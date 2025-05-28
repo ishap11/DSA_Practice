002f2f3452340a1b4870cc9dc2be53a0f9b0c77e
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-05-28 18:10:48 +0530

    Time: 0 ms (100%), Space: 42.8 MB (34.67%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -9,104 +9,29 @@
  * }
  */
 class Solution {
-    /*
-    public static class ListNode {
-        ListNode head;
-        ListNode tail;
-        int size; 
-    
-        void addLast(int val){
-            ListNode temp = new ListNode();
-            temp.val= val;
-            temp.next= null;
-            
-            if(size == 0){
-            head = tail = temp;
-            }else{
-            tail.next = temp ; 
-            tail = temp;
-            }
-            size++;
-    
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+        if(list1 == null || list2 == null) {
+            return list1 != null ? list1 : list2;
         }
-    
-    */
+        ListNode ans = new ListNode(-1);
+        ListNode res = ans;
 
-        public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode l1 = list1;
+        ListNode l2 = list2;
 
-        /* 
-        //     ListNode res = new ListNode(0);
-
-        //     while(list1 != null && list2 != null){
-        //         if(list1.val < list2.val){
-        //         res.addLast(list1.val);
-        //         list1 = list1.next;
-        //         }
-        //         else{
-        //         res.addLast(list2.val);
-        //         list2 = list2.next;
-        //         } 
-        //     }
-        //     while(list1 != null){
-        //         res.addLast(list1.val);
-        //         list1 =list1.next;
-        //     }
-        //     while(list2 != null){
-        //         res.addLast(list2.val);
-        //         list2 = list2.next;
-        //     }
-        //     return res;
-        // }
-        // recursive approach   TC- O(n+m) SC -O(n+m)
-        // if(list1 == null){
-        //     return list2;
-        // }
-        // if(list2 == null){
-        //     return list1;
-        // }
-        // if(list1.val <list2.val){
-        //     list1.next = mergeTwoLists(list1.next , list2);
-        //     return list1;
-        // }else{
-        //     list2.next = mergeTwoLists(list2.next , list1);
-        //     return list2;
-        */
-
-
-        // ANOTHER APPROACH
-            if(list1 == null) return list2;
-            if(list2 == null) return list1;
-
-            ListNode head = null;
-            ListNode tail = null;
-
-            while(list1 != null && list2 != null){
-                ListNode temp = null;
-
-                if(list1.val < list2.val){
-                    temp = list1;
-                    list1 = list1.next;
-                }else{
-                    temp = list2;
-                    list2 =  list2.next;
-                }
-                temp.next = null;
-                if(head == null) {
-                    head = tail = temp;
-                }else{
-                    tail.next = temp;
-                    tail = temp;
-                }
+        while(l1 != null && l2 != null) {
+            if(l1.val < l2.val) {
+                res.next = l1;
+                l1 = l1.next;
+            }else {
+                res.next = l2;
+                l2 = l2.next;
             }
-
-            if(list1 == null){
-                tail.next = list2;
-            }
-            if(list2 == null) {
-                tail.next = list1;
-            }
-            return head;
+            res =res.next;
         }
-        
-    /*}*/
+
+        res.next = l1 != null ? l1 : l2;
+        return ans.next;
+    }
 }
