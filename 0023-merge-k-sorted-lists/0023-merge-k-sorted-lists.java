f3b73cc8725001597895a9f966287d0413d95f0d
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-06-02 21:10:20 +0530

    Time: 4 ms (74.06%), Space: 44.2 MB (89.44%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -9,40 +9,40 @@
  * }
  */
 class Solution {
-    public ListNode mergeTwoSortedLL(ListNode l1 , ListNode l2) {
-        if(l1 == null || l2 == null) return l1 != null ? l1 : l2;
+    // public ListNode mergeTwoSortedLL(ListNode l1 , ListNode l2) {
+    //     if(l1 == null || l2 == null) return l1 != null ? l1 : l2;
 
-        ListNode dummy = new ListNode(-1);
-        ListNode prev = dummy;
+    //     ListNode dummy = new ListNode(-1);
+    //     ListNode prev = dummy;
 
-        ListNode c1 = l1 ;
-        ListNode c2 = l2;
+    //     ListNode c1 = l1 ;
+    //     ListNode c2 = l2;
 
-        while(c1 != null && c2 != null) {
-            if(c1.val < c2.val) {
-                prev.next = c1;
-                c1 = c1.next;
-            } else {
-                prev.next = c2;
-                c2 = c2.next;
-            }
-            prev = prev.next;
-        }
+    //     while(c1 != null && c2 != null) {
+    //         if(c1.val < c2.val) {
+    //             prev.next = c1;
+    //             c1 = c1.next;
+    //         } else {
+    //             prev.next = c2;
+    //             c2 = c2.next;
+    //         }
+    //         prev = prev.next;
+    //     }
 
-        prev.next = c1!= null ? c1 : c2;
+    //     prev.next = c1!= null ? c1 : c2;
 
-        return dummy.next;
-    }
-    public ListNode mergeKList(ListNode[] lists , int si , int ei) {
-        if(si > ei) return null;
-        if(si == ei) return lists[ei];
-        int mid = (si + ei)/2;
+    //     return dummy.next;
+    // }
+    // public ListNode mergeKList(ListNode[] lists , int si , int ei) {
+    //     if(si > ei) return null;
+    //     if(si == ei) return lists[ei];
+    //     int mid = (si + ei)/2;
 
-        ListNode l1 = mergeKList(lists , si , mid);
-        ListNode l2 = mergeKList(lists , mid + 1 , ei);
+    //     ListNode l1 = mergeKList(lists , si , mid);
+    //     ListNode l2 = mergeKList(lists , mid + 1 , ei);
 
-        return mergeTwoSortedLL(l1  , l2);
-    }
+    //     return mergeTwoSortedLL(l1  , l2);
+    // }
 
 
 
@@ -50,9 +50,29 @@ class Solution {
 
     // main function
     public ListNode mergeKLists(ListNode[] lists) {
-        if(lists.length == 0) return null;
+    //     if(lists.length == 0) return null;
 
-        return mergeKList(lists , 0 , lists.length - 1);
-    }
+    //     return mergeKList(lists , 0 , lists.length - 1);
     
+    
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((a, b) -> a.val - b.val);
+
+        for(ListNode list : lists) {
+            if(list != null) pq.add(list);
+        } 
+
+        ListNode dummy = new ListNode(-1);
+        ListNode prev = dummy;
+
+        while(pq.size() != 0) {
+            ListNode node =pq.remove();
+            prev.next = node;
+            prev = prev.next;
+            node = node.next;
+
+            if(node != null) pq.add(node);
+        }
+
+        return dummy.next;
+    }
 }
