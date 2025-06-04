c625a81974cddbfeca884ba9e55d9cd07fda3e5a
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-06-05 00:30:09 +0530

    Time: 2 ms (5.57%), Space: 57.1 MB (56.96%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,16 +1,15 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int n = nums.length ;
+        int n = nums.length;
         int ans = Integer.MIN_VALUE;
-        int sum  = 0;
+        int sum =0 ;
 
-        for(int i=0; i< n ;i++) {
+        for(int i=0; i< n ; i++) {
             sum += nums[i];
-            ans = Math.max(ans , sum);
 
-            if(sum < 0) {
-                sum = 0;
-            }
+            ans = Math.max(ans  , sum);
+
+            if(sum < 0) sum = 0;
         }
         return ans;
     }
