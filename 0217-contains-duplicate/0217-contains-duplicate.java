2e2e7636c57e74b7bfbd7f154a3f29e9f5f8794e
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-06-05 00:35:57 +0530

    Time: 13 ms (78.83%), Space: 58.1 MB (74.88%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,15 +1,22 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashMap<Integer , Integer> map = new HashMap<>();
+        // HashMap<Integer , Integer> map = new HashMap<>();
 
-        for(int num : nums) {
-            map.put(num , map.getOrDefault(num , 0)+1);
-        }
+        // for(int num : nums) {
+        //     map.put(num , map.getOrDefault(num , 0)+1);
+        // }
 
-        for(int freq : map.values()) {
-            if(freq >= 2) {
-                return true;
-            }
+        // for(int freq : map.values()) {
+        //     if(freq >= 2) {
+        //         return true;
+        //     }
+        // }
+        // return false;
+
+         HashSet<Integer> set= new HashSet<>();
+        for(int i: nums) {
+            if(set.contains(i)) return true;
+            set.add(i);
         }
         return false;
     }
