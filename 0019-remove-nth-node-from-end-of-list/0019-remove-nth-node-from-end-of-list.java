7ab84d2a8733bdba29f627c8052051320125585d
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-06-04 22:36:07 +0530

    Time: 0 ms (100%), Space: 42.4 MB (8.2%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -8,38 +8,66 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+/*
+reverse --> 5-----> 4-----> 3-----> 2----->1 */
+    
+    /*Eg :        1 -----> 2-----> 3-----> 4-----> 5
+STEP 1:  null     head
+STEP 2:  prev     curr     next
+STEP 3:       <----1        2 -----> 3------>4 ------>5
+                  prev     curr    next
+STEP 4:       <-----1 <------2       3 ------->4 ----->5
+                             prev     curr     next
+LAST STEP : 1 <----- 2 <------ 3 < ------ 4 <------5
+*/ 
 class Solution {
-    public ListNode reverse(ListNode head) {
+
+    // reverse 
+    private ListNode reverse(ListNode head) {
+
+        // base case
         if(head == null || head.next == null) {
             return head;
         }
 
         ListNode curr = head;
-        ListNode prev =null;
+        ListNode prev = null;
 
         while(curr != null) {
-            ListNode next =curr.next;
+            ListNode next = curr.next;
             curr.next = prev;
             prev = curr;
             curr = next;
         }
         return prev;
     }
-
     public ListNode removeNthFromEnd(ListNode head, int n) {
+       
+        // calling the method reverse and passing the argument head
+        //  in return i got ----> 5 ----> 4 -----> 3 -----> 2 -----> 1
         ListNode reverseList = reverse(head);
 
-       ListNode dummy = new ListNode(-1);
-       dummy.next = reverseList;
+        // -1     5 ----> 4 -----> 3 -----> 2 -----> 1
+        ListNode dummy = new ListNode(-1);
 
-       ListNode curr = dummy;
+        //  -1 (dummy) -----> 5 ----> 4 -----> 3 -----> 2 -----> 1
+        dummy.next = reverseList;
 
-       for(int i=1 ; i< n ; i++) {
-        curr = curr.next;
-       }
+         //  -1 (dummy || curr) -----> 5 ----> 4 -----> 3 -----> 2 -----> 1
+        ListNode curr = dummy;
 
-       curr.next = curr.next != null ? curr.next.next : null;
+        //1} -1 [i](dummy || curr) -----> 5 ----> 4 -----> 3 -----> 2 -----> 1
+        //2} -1 (dummy  -----> 5(curr)[i] ----> 4 -----> 3 -----> 2 -----> 1
+        
+        for(int i= 1 ; i< n ; i++) {
+            curr = curr.next;
+        }
 
-       return reverse(dummy.next);
+        curr.next = curr.next != null ? curr.next.next : null;
+
+        
+        return reverse(dummy.next);
+
     }
 }
