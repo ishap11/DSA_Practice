98fcf42ea8b279591cb8f15f2e6cd7dbe0cdb872
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-06-05 14:57:43 +0530

    Time: 0 ms (100%), Space: 42.2 MB (53.47%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,29 +1,32 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int low  = 0;
-        int high = nums.length - 1;
+        int lo = 0;
+        int hi = nums.length -1;
 
-        while(low <= high) {
-            int mid = (low + high) / 2;
+        while(lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
 
-            if (nums[mid] == target) {
+             if (nums[mid] == target) {
                 return mid;
             }
 
-            // left half is sorted......nums[low] <= target <= nums[mid]
-            if(nums[low] <= nums[mid] ) {
-                if(target >= nums[low] && target <= nums[mid]) { // target exist in left side
-                    high = mid - 1;
-                } else {
-                    low = mid + 1;
+            // left half sorted 
+            if(nums[lo] <= nums[mid]) {
+                // target exist in b/w 
+                if(nums[lo] <= target && target <= nums[mid]) {
+                    hi = mid - 1;
+                }
+                // does not exist 
+                else {
+                    lo = mid + 1;
                 }
             } 
-            // right half is sorted. (nums[mid] <= nums[high]).....nums[mid] <= target <= nums[high]
+            // right part sorted
             else {
-                if(target > nums[mid] && target <= nums[high] ) { // target exist in right side 
-                    low = mid + 1;
+                if(nums[mid] <= target && target <= nums[hi]) {
+                    lo = mid + 1;
                 } else {
-                    high = mid - 1;
+                    hi = mid - 1;
                 }
             }
         }
