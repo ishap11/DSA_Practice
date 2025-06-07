178edc99f38eba85d84efda0c647b17f588a6de1
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-06-07 18:17:39 +0530

    Time: 0 ms (100%), Space: 43.4 MB (21.16%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,49 +1,40 @@
 class Solution {
     public void nextPermutation(int[] nums) {
         int i = nums.length - 2;
-        
-        // Step 1: Find first decreasing element from the end
+
         while(i>= 0 && nums[i] >= nums[i+1]) {
             i--;
         }
-
-        if(i == -1){
-            // reverse the largest to the smallest 
-            int s = nums.length -1;
-            int t = 0;
-
-            while(t<s) {
+        if(i == -1) {
+            int t = nums.length - 1;
+            int s = 0;
+            while(s<t) {
                 int temp = nums[t];
                 nums[t] = nums[s];
                 nums[s] = temp;
-                t++;
-                s--;
+                s++;
+                t--;
             }
             return;
         }
 
-        // Step 2: Find next larger element on the right
-        int k = nums.length -1;
-        while(nums[i] >= nums[k]) {
+        int k = nums.length - 1;
+        while( nums[i] >= nums[k]) {
             k--;
         }
-        // swap 
         int temp = nums[i];
         nums[i] = nums[k];
         nums[k] = temp;
 
-
-        // Reverse the suffix(jisse smallest no bnjaaye)
-        int start = i + 1;
+        int st = i + 1;
         int end = nums.length - 1;
 
-        while (start < end) {
-            int tempVar = nums[start];
-            nums[start] = nums[end];
-            nums[end] = tempVar;
-            start++;
+        while(st < end) {
+            int tempp = nums[st];
+            nums[st] = nums[end];
+            nums[end] = tempp;
+            st++;
             end--;
         }
-
     }
 }
