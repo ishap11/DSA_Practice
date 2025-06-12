bdcb7fb72abc7b78dca16a3798adb5560c213f2e
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-06-12 19:31:05 +0530

    Time: 0 ms (100%), Space: 44.6 MB (35.67%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -14,44 +14,38 @@
  * }
  */
 class Solution {
-    public int kthSmallest(TreeNode root, int k) {
-        // TC  - O(k) SC - O(h)
-        Stack<TreeNode> st = new Stack<>();
-
-        while (root != null || !st.isEmpty()) {
-            while (root != null) {
-                st.push(root);
-                root = root.left;
-            }
-            
-            root = st.pop();
-            
-            // Decrement k, as we've visited one more node
-            k--;
-            
-            // If k is 0, we've found the k-th smallest element
-            if (k == 0) {
-                return root.val;
-            }
-            root = root.right;
+    public TreeNode getRightMostNode(TreeNode leftNode , TreeNode curr) {
+        while(leftNode.right != null && leftNode.right != curr) {
+            leftNode = leftNode.right;
         }
-         throw new IllegalArgumentException("k is larger than the number of nodes in the tree");
-
-
-        // TC- O(n) Sc - O(n)
-        // List<Integer> ans = new ArrayList<>();
-        // inorder(root , ans);
-
-        // return ans.get(k - 1);
-
+        return leftNode;
     }
 
-    // public void inorder(TreeNode root , List<Integer> ans ){
-    //     if(root == null){
-    //         return ;
-    //     }
-    //     inorder(root.left , ans);
-    //     ans.add(root.val);
-    //     inorder(root.right , ans);
-    // }
+    public int kthSmallest(TreeNode root, int k) {
+         int count = 0;
+        TreeNode curr = root;
+        
+        while(curr != null) {
+            TreeNode leftNode = curr.left;
+            
+            if(leftNode == null) {
+                count++;
+                if(count == k) return curr.val;
+                curr = curr.right;
+            }else {
+                TreeNode rightMostNode = getRightMostNode(leftNode , curr);
+                
+                if(rightMostNode.right == null) {
+                    rightMostNode.right = curr;
+                    curr = curr.left;
+                } else{
+                    rightMostNode.right = null;
+                    count++; 
+                    if(count == k) return curr.val;
+                    curr = curr.right;
+                }
+            }
+        }
+        return -1;
+    }
 }
