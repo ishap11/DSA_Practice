09e1501279c3a19a3ea8cca0b8cd2aee6c35b093
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-06-15 16:15:56 +0530

    Time: 0 ms (100%), Space: 42.9 MB (94.44%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,6 +1,6 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int i= nums.length - 2;
+        int i = nums.length -2;
 
         while(i>= 0 && nums[i] >= nums[i+1]) {
             i--;
@@ -8,6 +8,7 @@ class Solution {
         if(i==-1) {
             int s = 0;
             int t = nums.length -1;
+
             while(s<=t) {
                 int temp = nums[s];
                 nums[s] = nums[t];
@@ -18,21 +19,20 @@ class Solution {
             return;
         }
 
-        int k =  nums.length - 1;
-
+        int k = nums.length - 1;
         while(nums[i] >= nums[k]) {
             k--;
         }
         int temp = nums[i];
-        nums[i]= nums[k];
-        nums[k]= temp;
+        nums[i] = nums[k];
+        nums[k] = temp;
 
         int st = i+1;
-        int end = nums.length -1;
+        int end = nums.length - 1;
 
         while(st <= end) {
-            int tempp = nums[st];
-            nums[st]  = nums[end];
+            int tempp= nums[st];
+            nums[st] = nums[end];
             nums[end] = tempp;
             st++;
             end--;
