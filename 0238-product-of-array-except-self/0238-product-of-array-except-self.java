f9f65e945821327f6dacd12b50e43303145cc210
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-06-17 09:33:34 +0530

    Time: 2 ms (89.06%), Space: 55.1 MB (94.34%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,24 +1,30 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int[] res = new int[nums.length];
+        // Brute force --TLE
+        // int n = nums.length;
 
-        int[] rightProd = new int[nums.length];
-        int mult = 1;
-        for(int i= nums.length -1 ; i>= 0 ; i--){
-           mult *= nums[i];
-           rightProd[i] = mult;
+        // int[] res = new int[n];
+        // Arrays.fill(res, 1);
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j < n; j++) {
+        //         if (i != j) {
+        //             res[i] *= nums[j];
+        //         }
+        //     }
+        // }
+        // return res;
+
+        int[] ans = new int[nums.length];
+        int prefix = 1;
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = prefix;
+            prefix *= nums[i];
         }
-
-        mult = 1;
-        for(int i=0 ; i< nums.length - 1; i++) {
-           int lp = mult;
-           int rp = rightProd[i+1];
-
-           res[i] = lp * rp;
-
-           mult *= nums[i];
+        int suffix = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            ans[i] *= suffix;
+            suffix *= nums[i];
         }
-        res[nums.length - 1] = mult;
-        return res;
+        return ans;
     }
 }
