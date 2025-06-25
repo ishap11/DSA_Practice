d62fee67a0a7e68ced4d6f28404c2c1af186ca29
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-06-25 17:42:47 +0530

    Time: 21 ms (46.3%), Space: 44.9 MB (49.75%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,30 +1,78 @@
 class Solution {
-    public boolean isPalindrome(String str) {
-        int left = 0;
-        int right = str.length() - 1;
-
-        while (left < right) {
-            if (str.charAt(left) != str.charAt(right))
-                return false;
-            left++;
-            right--;
-        }
-
-        return true;
-        
-    }
     public String longestPalindrome(String s) {
-        if (s == null || s.length() < 1) return "";
-        String longest = "";
+        String res = "";
 
-        for(int i=0 ; i< s.length() ; i++){
-            for(int j= i ; j< s.length() ; j++) {
-                String sub = s.substring(i , j+1);
-                if (isPalindrome(sub) && sub.length() > longest.length()) {
-                    longest = sub;
+        // odd case
+        for(int ax = 0; ax < s.length() ; ax++){
+            int orb = 1;
+            int len = 1;
+
+            while(ax - orb >= 0 && ax + orb < s.length()){
+                if(s.charAt(ax - orb) == s.charAt(ax + orb)) {
+                    orb++;
+                    len += 2;
+                }else{
+                    break;
                 }
             }
+
+            if(len > res.length()) {
+                int si = ax - len/2;
+                res = s.substring(si , si + len);
+            }
         }
-        return longest;
+
+        // even 
+        for(int ax = 0; ax < s.length() - 1 ; ax++){
+            int orb = 1;
+            int len = 0;
+
+            while(ax - orb + 1 >= 0 && ax + orb < s.length()) {
+                if(s.charAt(ax - orb + 1) == s.charAt(ax + orb)) {
+                    orb++;
+                    len += 2;
+                }else{
+                    break;
+                }
+            }
+
+            if(len > res.length()) {
+                int si = ax - len/2 + 1;
+                res = s.substring(si , si + len);
+            }
+        }
+
+        return res;
+
+
+
+        // ~~~~~~~~~~~~~~~~~~~~~ APPROACH - 1 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        // if (s == null || s.length() < 1) return "";
+        // String longest = "";
+
+        // for(int i=0 ; i< s.length() ; i++){
+        //     for(int j= i ; j< s.length() ; j++) {
+        //         String sub = s.substring(i , j+1);
+        //         if (isPalindrome(sub) && sub.length() > longest.length()) {
+        //             longest = sub;
+        //         }
+        //     }
+        // }
+        // return longest;
     }
+
+    // public boolean isPalindrome(String str) {
+    //     int left = 0;
+    //     int right = str.length() - 1;
+
+    //     while (left < right) {
+    //         if (str.charAt(left) != str.charAt(right))
+    //             return false;
+    //         left++;
+    //         right--;
+    //     }
+
+    //     return true;
+        
+    // }
 }
