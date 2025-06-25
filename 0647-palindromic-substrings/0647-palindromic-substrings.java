756a4ab4c3b010c1bb2339a4bc8e73474557d410
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-06-25 22:01:18 +0530

    Time: 6 ms (70.48%), Space: 41.6 MB (73.31%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,29 +1,52 @@
 class Solution {
-    public int countSubstrings(String s) {
-        int count =  0;
-        for(int i =0 ; i< s.length() ; i++) {
-            for(int j= i+1  ; j<= s.length() ; j++) {
-                String sub = s.substring(i , j);
 
-                if (isPalindrome(sub)) {
-                    count++;
-                }
-            }
+    private int isPalindrome(String s , int i , int j) {
+        int count =  0;
+
+        while(i>= 0 && j< s.length() && s.charAt(i) == s.charAt(j)) {
+            count++;
+            i--;
+            j++;
         }
         return count;
     }
 
-    public boolean isPalindrome(String str) {
-        int left = 0;
-        int right = str.length() - 1;
+    public int countSubstrings(String s) {
+        int count = 0;
 
-        while(left < right) {
-            if(str.charAt(left) != str.charAt(right) ) {
-                return false;
-            }
-            left++;
-            right--;
+        for(int i=0; i< s.length() ; i++) {
+
+            count+= isPalindrome(s , i , i);
+            count+= isPalindrome(s , i , i+1);
         }
-        return true;
+        return count;
+
+
+
+        // int count =  0;
+        // for(int i =0 ; i< s.length() ; i++) {
+        //     for(int j= i+1  ; j<= s.length() ; j++) {
+        //         String sub = s.substring(i , j);
+
+        //         if (isPalindrome(sub)) {
+        //             count++;
+        //         }
+        //     }
+        // }
+        // return count;
     }
+
+    // public boolean isPalindrome(String str) {
+    //     int left = 0;
+    //     int right = str.length() - 1;
+
+    //     while(left < right) {
+    //         if(str.charAt(left) != str.charAt(right) ) {
+    //             return false;
+    //         }
+    //         left++;
+    //         right--;
+    //     }
+    //     return true;
+    // }
 }
