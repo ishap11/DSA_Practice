82b34b304468e01589b8da7437e903da5da5e924
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-06-26 21:47:40 +0530

    Time: 165 ms (8.84%), Space: 45.7 MB (21.44%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,61 +1,60 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if (s.length() < t.length()) return "";
+        if(s.length() < t.length()) return "";
 
         String ans ="";
 
-        HashMap<Character , Integer> map2 = new HashMap<>();
-        for(int i=0; i< t.length() ; i++){
+        HashMap<Character , Integer> map1 = new HashMap<>();
+
+        for(int i=0 ; i < t.length() ; i++) {
             char ch = t.charAt(i);
-            map2.put(ch , map2.getOrDefault(ch,0) +1);
+            map1.put(ch , map1.getOrDefault(ch , 0) + 1);
         }
 
-        int mct =0;
+        int mct = 0;
         int dmct = t.length();
 
-        HashMap<Character , Integer> map1 = new HashMap<>();
+        HashMap<Character , Integer> map2 = new HashMap<>();
+
         int i= -1;
-        int j= -1;
-        
-        while(true){
+        int j = -1;
+
+        while(true) {
             boolean f1 = false;
             boolean f2 = false;
-            // acquire
-            while(i< s.length()-1 && mct < dmct ){
+
+            while(i< s.length() - 1 && mct < dmct) {
                 i++;
                 char ch = s.charAt(i);
-                map1.put(ch , map1.getOrDefault(ch,0) +1);
+                map2.put(ch , map2.getOrDefault(ch , 0) + 1);
 
-                if(map1.get(ch).intValue() <= map2.getOrDefault(ch, 0).intValue() ){
+                if(map2.getOrDefault(ch , 0) <= map1.getOrDefault(ch , 0)) {
                     mct++;
                 }
                 f1 = true;
             }
-         
-
-            // collect ans and release
-            while(j < i && mct == dmct){
-                String pans = s.substring(j + 1, i + 1);
-                
-                if(ans.isEmpty()|| pans.length() < ans.length()){
+            
+            while(j < i && mct == dmct ) {
+                String pans = s.substring(j+1, i+1);
+                if(ans.length() == 0 || pans.length() < ans.length()) {
                     ans = pans;
                 }
+
                 j++;
-
                 char ch = s.charAt(j);
-
-                if(map1.get(ch) == 1){
-                    map1.remove(ch);
-                }else{
-                    map1.put(ch , map1.get(ch) - 1);
+                if(map2.get(ch) == 1) {
+                    map2.remove(ch);
+                } else{
+                    map2.put(ch , map2.get(ch) - 1);
                 }
-                if(map1.getOrDefault(ch,0)  < map2.getOrDefault(ch,0) ){
+
+                if(map2.getOrDefault(ch ,0) < map1.getOrDefault(ch , 0)) {
                     mct--;
                 }
                 f2 = true;
             }
-            if (!f1 && !f2) break;
+            if(f1 == false && f2 == false) break;
         }
-        return ans;   
+        return ans;     
     }
 }
