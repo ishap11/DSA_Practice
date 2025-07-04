369a6c08a6b55114a51dc67857217da05770405f
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-07-04 17:12:57 +0530

    Time: 4 ms (56.39%), Space: 43.7 MB (51.77%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,38 +1,25 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        // String str = s.toLowerCase().replaceAll("[^a-zA-Z0-9]" , "");
-        // int i= 0;
-        // int j = str.length() -1;
-
-        // while(i<= j) {
-        //     if(str.charAt(i) != str.charAt(j)) {
-        //         return false;
-        //     }else{
-        //         i++;
-        //         j--;
-        //     }
-        // }
-        // return true;
-
         StringBuilder sb = new StringBuilder();
-        
-        for(char c : s.toCharArray()) {
-            if (Character.isLetterOrDigit(c)) {
-                sb.append(Character.toLowerCase(c));
+
+        for(char ch : s.toCharArray()) {
+            if(Character.isLetterOrDigit(ch)) {
+                sb.append(Character.toLowerCase(ch));
             }
         }
         String str = sb.toString();
-        int i= 0;
-        int j = str.length() -1;
+        int i=0;
+        int j = str.length() - 1;
 
-        while(i<= j) {
-            if(str.charAt(i) != str.charAt(j)) {
-                return false;
-            }else{
+        while(i<j) {
+            if(str.charAt(i) == str.charAt(j)) {
                 i++;
                 j--;
+            }else {
+                return false;
             }
         }
+
         return true;
     }
 }
