a59fe711c4ec8df9b4dcef70713ea8d6af5eb7be
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-07-07 21:29:20 +0530

    Time: 30 ms (69.46%), Space: 51.3 MB (92.22%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,43 +1,51 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans= new ArrayList<>();
-        Arrays.sort(nums);
-        HashSet<Long> set = new HashSet<>();
+        // Set<List<Integer>> set = new HashSet<>();
+        // int n = nums.length;
 
-        int tar = 0;
-        for(int i=0; i< nums.length ; i++){
-            int newtar = tar - nums[i];
-            int li = i+1;
-            int ri = nums.length -1;
-            while(li < ri){
-                if(nums[li] + nums[ri] < newtar){
-                    li++;
-                }else if(nums[li] + nums[ri] > newtar){
-                    ri--;
-                }else{
-                    long hash = getHash(nums[i] , nums[li] , nums[ri]);
-                    if(set.contains(hash) == false){
-                        ans.add(Arrays.asList(nums[i] , nums[li] , nums[ri]));
-                        set.add(hash);
-                    }
-                    
-                    li++;
-                    ri--;
+        // for(int i= 0 ; i< n  ; i++) {
+        //     for(int j = i+1 ; j< n ; j++) {
+        //         for(int k= j+1 ; k< n ;k++) {
+        //             if(nums[i] + nums[j] + nums[k] == 0) {
+        //                 List<Integer> ans = Arrays.asList(nums[i], nums[j], nums[k]);
+        //                 Collections.sort(ans);
+        //                 set.add(ans);
+        //             }
+        //         }
+        //     }
+        // }
+
+        // return new ArrayList<>(set);
+
+        List<List<Integer>> ans = new ArrayList<>();
+        int n = nums.length ; 
+        Arrays.sort(nums);
+
+        for(int i=0 ; i< nums.length ; i++) {
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+
+            int left = i+1;
+            int right = n-1;
+
+            while(left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if(sum == 0) {
+                    ans.add(Arrays.asList(nums[i] , nums[left] , nums[right]));
+
+                    while(left < right && nums[left] == nums[left+1]) left++;
+                    while(left < right && nums[right] == nums[right - 1]) right--;
+
+                    left++;
+                    right--; 
+                }
+                else if(sum < 0) {
+                    left++;
+                }else {
+                    right--;
                 }
             }
         }
         return ans;
-
-    }
-    public long getHash(int x , int y , int z){
-        long hash = x;
-        hash *= 100000;
-
-        hash += y;
-        hash *= 100000;
-
-        hash+=z;
-        hash *=100000;
-        return hash;
     }
 }
