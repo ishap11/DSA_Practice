1fb44384060f9d2a8ba37bc4ed201d4401ab69bc
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-07-09 14:09:08 +0530

    Time: 0 ms (100%), Space: 41.1 MB (78.63%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.java
+++ 0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -15,25 +15,6 @@
  */
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-        // if(root == null) return null;
-
-        // Stack<TreeNode> st = new Stack();
-
-        // st.push(root);
-
-        // while(!st.isEmpty()) {
-        //     root = st.pop();
-
-        //     TreeNode temp = root.left;
-        //     root.left = root.right;
-        //     root.right = temp;
-
-        //     if(root.left != null) invertTree(root.left);
-        //     if(root.right != null) invertTree(root.right);
-        // }
-
-        // return root;
-
         if(root == null) return null;
 
         TreeNode temp = root.left;
@@ -42,7 +23,7 @@ class Solution {
 
         invertTree(root.left);
         invertTree(root.right);
-        
+
         return root;
     }
 }
