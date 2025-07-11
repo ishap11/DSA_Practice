c3b0722509e6743d24c3196cc3877bb76e7253da
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-07-12 04:22:27 +0530

    Time: 7 ms (62.12%), Space: 45.2 MB (19.54%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -9,17 +9,15 @@
  */
 class Solution {
     public TreeNode LCA = null;
-    public boolean LCA_DFS(TreeNode root , TreeNode p , TreeNode q){
+
+    private boolean lca(TreeNode root , TreeNode p , TreeNode q) {
         if(root == null) return false;
         boolean self = (root.val == p.val || root.val == q.val);
-        boolean left = LCA_DFS(root.left ,  p,q);
-        if(LCA != null) {
-            return true;
-        }
-        boolean right = LCA_DFS(root.right ,  p,q);
-        if(LCA != null) {
-            return true;
-        }
+        boolean left = lca(root.left , p , q);
+        if(LCA != null) return true;
+
+        boolean right = lca(root.right , p  , q);
+        if(LCA != null) return true;
 
         if((left && right ) || (left && self) || (right && self)) 
             LCA = root;
@@ -27,7 +25,7 @@ class Solution {
         return self || left || right;
     }
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        LCA_DFS(root , p ,q);
-        return LCA;
+        lca(root , p , q);
+        return LCA; 
     }
 }
