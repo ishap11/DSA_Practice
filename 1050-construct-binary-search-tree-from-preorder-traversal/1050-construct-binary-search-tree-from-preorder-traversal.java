e81d58d7e6dd6193f5e00adda2ca738d7f4f349a
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-07-15 21:44:52 +0530

    Time: 0 ms (100%), Space: 41.9 MB (30.16%) - LeetHub

--- 1050-construct-binary-search-tree-from-preorder-traversal/1050-construct-binary-search-tree-from-preorder-traversal.java
+++ 1050-construct-binary-search-tree-from-preorder-traversal/1050-construct-binary-search-tree-from-preorder-traversal.java
@@ -14,9 +14,10 @@
  * }
  */
 class Solution {
-    private int idx = 0;
-     private TreeNode bstFromPreorder(int[] preorder, int lr, int rr) {
-        if (idx >= preorder.length || preorder[idx] < lr || preorder[idx] > rr) {
+    private int idx =0;
+
+    private TreeNode bstFromPreorder(int[] preorder , int lr , int rr){
+        if(idx >= preorder.length || preorder[idx] < lr || preorder[idx] > rr){
             return null;
         }
 
@@ -26,7 +27,6 @@ class Solution {
 
         return node;
     }
-
     public TreeNode bstFromPreorder(int[] preorder) {
         return bstFromPreorder(preorder, Integer.MIN_VALUE, Integer.MAX_VALUE);
     }
