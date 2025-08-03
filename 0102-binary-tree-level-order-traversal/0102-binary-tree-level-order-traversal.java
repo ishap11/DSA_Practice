15735f283cd1f9d8f09691705d79c7a8633f89b2
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-08-03 19:38:09 +0530

    Time: 1 ms (91.49%), Space: 45.1 MB (35.2%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -15,62 +15,31 @@
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        // List<List<Integer>> res = new ArrayList<>();
-        // if (root == null) return res;
-
-        // Queue<TreeNode> queue = new LinkedList<>();
-        // queue.add(root);
-
-        // while (!queue.isEmpty()) {
-        //     int levelsize = queue.size();
-        //     List<Integer> currlevel = new ArrayList<>();
-
-        //     for (int i = 0; i < levelsize; i++) {
-        //         TreeNode currNode = queue.poll();
-
-        //         currlevel.add(currNode.val);
-        //         if (currNode.left != null) queue.add(currNode.left);
-        //         if (currNode.right != null) queue.add(currNode.right);
-        //     }
-
-        //     res.add(currlevel);
-        // }
-
-        // return res;
-
-
         List<List<Integer>> ans = new ArrayList<>();
+
         if (root == null) return ans;
 
-        Queue<TreeNode> queue = new ArrayDeque<>();
-        queue.add(root);
+        Queue<TreeNode> que = new ArrayDeque<>();
+        que.add(root);
 
-        while (!queue.isEmpty()) {
-            int count = queue.size();
+        while(que.size() > 0) {
+            int count = que.size();
 
-            // Create a temporary list to store values of nodes at this level.
-            List<Integer> currLevel = new ArrayList<>();
+            List<Integer> res = new ArrayList<>();
 
-            // Process each node at the current level.
-            for (int i = 0; i < count; i++) {
-                TreeNode currNode = queue.remove(); // Dequeue the front node.
-                
-                // Add the value of the current node to the current level's list.
-                currLevel.add(currNode.val);
+            for(int i=0; i< count ; i++) {
+                root = que.remove();
+                res.add(root.val);
 
-                // If the left child exists, enqueue it for the next level.
-                if (currNode.left != null) {
-                    queue.add(currNode.left);
+                if(root.left != null) {
+                    que.add(root.left);
                 }
-
-                // If the right child exists, enqueue it for the next level.
-                if (currNode.right != null) {
-                    queue.add(currNode.right);
+                if(root.right != null) {
+                    que.add(root.right);
                 }
             }
 
-            // After processing all nodes at the current level, add the level's list to the result.
-            ans.add(currLevel);
+            ans.add(res);
         }
         return ans;
     }
