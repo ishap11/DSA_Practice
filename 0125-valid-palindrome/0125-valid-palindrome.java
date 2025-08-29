44bb2199961204349601c8877ff6e5ea45059533
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-08-29 05:48:37 +0530

    Time: 3 ms (72.02%), Space: 43.6 MB (55.76%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -7,11 +7,13 @@ class Solution {
                 sb.append(Character.toLowerCase(ch));
             }
         }
-        String str = sb.toString();
-        int i=0;
-        int j = str.length() - 1;
 
-        while(i<j) {
+        String str = sb.toString();
+
+        int i=0;
+        int j= str.length() -1 ;
+
+        while(i<=j) {
             if(str.charAt(i) == str.charAt(j)) {
                 i++;
                 j--;
