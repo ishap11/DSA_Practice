35b9e77c48804de4652a1a53f7fd372f2424759f
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-08-29 05:58:02 +0530

    Time: 5 ms (65.65%), Space: 44.1 MB (46.69%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,41 +1,21 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        // TC- O(N) SC - O(1)
-        // if(x < 0 ){
-        //     return false; //since -ve no. can't be a palindrome
-        // }
-        // int temp = x;
-        // int reverseNum = 0;
-        // while(temp != 0){
-        //     int digit = temp  %10;
-        //     reverseNum = reverseNum *10 +  digit;
-        //     temp = temp/10;
-        // }
-        
-        // if(reverseNum == x){
-        //     return true;
-        // }else{
-        //     return false;
-        // }
-
-
-        // Again solving
-        //TC - O(n) SC - O(1)
-        if(x< 0){
+        if(x < 0) {
             return false;
         }
-        if(x ==0 ){
-            return true;
-        }
 
-        int ans =0;
-        int orgnlNo = x;
+        if(x == 0) return true;
 
-        while(x != 0){
-            int rem = x%10;
+        int originalNo = x;
+        int ans = 0;
+
+        while(x != 0) {
+            int rem = x % 10;
             x = x/10;
-            ans = ans*10 + rem ;
+
+            ans = ans * 10 + rem;
         }
-        return ans == orgnlNo;
+
+        return ans == originalNo;
     }
 }
