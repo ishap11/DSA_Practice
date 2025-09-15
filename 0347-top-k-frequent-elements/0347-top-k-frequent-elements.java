fabba2073912381a041b6fe8eaf07e65f302895a
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-09-15 06:23:34 +0530

    Time: 57 ms (5.23%), Space: 46.3 MB (82.06%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,15 +1,15 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        int[] ans = new int[k];
+        int[] ans =new int[k];
+        HashMap<Integer , Integer> map = new HashMap<>();
 
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        for(int i=0 ; i< nums.length ; i++) {
-            map.put(nums[i] , map.getOrDefault(nums[i] , 0) + 1);
+        for(int num : nums){
+            map.put(num , map.getOrDefault(num , 0) + 1);
         }
-        for(int i=0 ; i< k ; i++) {
-            int maxFreq =  0;
+
+        for(int i=0 ; i< k ; i++){
             int maxKey = 0;
+            int maxFreq = 0;
 
             for(int key : map.keySet()) {
                 if(map.get(key) > maxFreq) {
@@ -17,7 +17,7 @@ class Solution {
                     maxKey = key;
                 }
             }
-
+            
             ans[i] = maxKey;
             map.remove(maxKey);
         }
