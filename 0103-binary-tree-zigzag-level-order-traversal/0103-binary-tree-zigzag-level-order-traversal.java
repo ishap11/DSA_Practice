8ecbfa3c86f71bdf2298f35610b67a3b0cf9fb9e
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-09-18 07:52:53 +0530

    Time: 1 ms (63.84%), Space: 42.2 MB (58.9%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -15,43 +15,41 @@
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> res = new ArrayList<>();
-        if (root == null) {
-            return res; 
-        }
+        List<List<Integer>> ans = new ArrayList<>();
 
-        Stack<TreeNode> st1 = new Stack<>(); 
-        Stack<TreeNode> st2 = new Stack<>(); 
-        boolean leftToRight = true; 
+        if(root == null) return ans;
+
+        Stack<TreeNode> st1 = new Stack<>();
+        Stack<TreeNode> st2 = new Stack<>();
+        boolean leftToRight = true;
 
         st1.push(root);
 
-        while (!st1.isEmpty()) {
+        while(!st1.isEmpty()) {
             List<Integer> level = new ArrayList<>();
 
-            while (!st1.isEmpty()) {
+            while(!st1.isEmpty()){
                 TreeNode node = st1.pop();
+
                 level.add(node.val);
 
-                // Add children to st2 in order depending on the traversal direction
-                if (leftToRight) {
-                    if (node.left != null) st2.push(node.left);
-                    if (node.right != null) st2.push(node.right);
-                } else {
-                    if (node.right != null) st2.push(node.right);
-                    if (node.left != null) st2.push(node.left);
+                if(leftToRight){
+                    if(node.left != null) st2.push(node.left);
+                    if(node.right != null) st2.push(node.right);
+                }else{
+                    if(node.right != null) st2.push(node.right);
+                    if(node.left != null) st2.push(node.left);
                 }
             }
+            ans.add(level);
 
-            res.add(level); // Add the current level to the result
-
-            // Swap stacks and toggle the direction
             Stack<TreeNode> temp = st1;
             st1 = st2;
-            st2 = temp;
+            st2 = temp; 
             leftToRight = !leftToRight;
         }
 
-        return res;
+        return ans;
+        
     }
 }
