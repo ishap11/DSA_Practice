7be99ad474d34fba58a3151945e6fa96f1c459fa
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-09-26 07:05:59 +0530

    Time: 166 ms (7.27%), Space: 46.1 MB (5.29%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -4,17 +4,15 @@ class Solution {
         String ans = "";
 
         HashMap<Character , Integer> map2 = new HashMap<>();
-        for(int i=0 ; i< t.length() ; i++){
+        for(int i = 0 ; i< t.length() ; i++){
             char ch = t.charAt(i);
-
-            map2.put(ch , map2.getOrDefault(ch , 0 ) + 1);
+            map2.put(ch , map2.getOrDefault(ch , 0) + 1);
         }
-
-        HashMap<Character , Integer> map1 = new HashMap<>();
 
         int mct = 0;
         int dmct = t.length();
 
+        HashMap<Character , Integer> map1 = new HashMap<>();
         int i = -1;
         int j = -1;
 
@@ -22,39 +20,40 @@ class Solution {
             boolean f1 = false;
             boolean f2 = false;
 
-            while(i < s.length() - 1 && mct < dmct){
+            while(i< s.length() - 1 && mct < dmct){
                 f1 = true;
                 i++;
 
                 char ch = s.charAt(i);
+                map1.put(ch , map1.getOrDefault(ch , 0) + 1);
 
-                map1.put(ch , map1.getOrDefault(ch,0) + 1);
-
-                if(map1.getOrDefault(ch , 0) <= map2.getOrDefault(ch ,0)) {
+                if(map1.getOrDefault(ch , 0) <= map2.getOrDefault(ch , 0)){
                     mct++;
                 }
             }
 
-            while(j<i && mct == dmct) {
+            while(j<i && dmct == mct){
                 f2 = true;
                 String pans = s.substring(j+1 , i+1);
-                if(ans.length() == 0 || pans.length() < ans.length()) {
+
+                if(ans.length() == 0 || ans.length() > pans.length()){
                     ans = pans;
                 }
-                j++;
 
+                j++;
                 char ch = s.charAt(j);
 
-                if(map1.get(ch) == 1) {
+                if(map1.get(ch) == 1){
                     map1.remove(ch);
-                } else {
-                    map1.put(ch  ,map1.get(ch) - 1);
+                }else {
+                    map1.put(ch , map1.get(ch) -1) ;
                 }
 
                 if(map1.getOrDefault(ch , 0) < map2.getOrDefault(ch , 0 )) {
                     mct--;
                 }
             }
+
             if(!f1 && !f2) break;
         }
         return ans;
