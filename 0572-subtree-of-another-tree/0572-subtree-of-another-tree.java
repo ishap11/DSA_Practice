d66c9652e6fc975cc949cfb798d492533e15c47b
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-09-29 05:59:05 +0530

    Time: 2 ms (98.13%), Space: 44.7 MB (21.79%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -14,23 +14,18 @@
  * }
  */
 class Solution {
-    public boolean isIdentical(TreeNode p, TreeNode q){
-        if(p == null && q== null) return  true;
-        else if( (p== null || q== null) || ( p.val != q.val ) ){
-            return false;
-        }
-        
-        return isIdentical(p.left, q.left) && isIdentical(p.right , q.right);
+    public boolean isIdentical(TreeNode p , TreeNode q){
+        if(p == null && q == null) return true;
+        if((p == null || q == null) || (p.val != q.val)) return false;
 
+        return isIdentical(p.left , q.left) && isIdentical(p.right , q.right);
     }
+
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if(root == null ) return false;
-        
+        if(root == null) return false;
+
         if(isIdentical(root , subRoot)) return true;
 
         return isSubtree(root.left , subRoot) || isSubtree(root.right , subRoot);
-
-
-
     }
 }
