06488cd6def18ed07eaeb3c2fe71c6c98944e249
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-10-01 06:12:59 +0530

    Time: 1 ms (100%), Space: 46.1 MB (61.51%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,90 +1,36 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         // BRUTE FORCCE APPROACH
-        // TC - O(n1 +n2) SC - O(n1+n2)
-
-        // int n1 = nums1.length;
-        // int n2 = nums2.length;
-        // int n = n1 + n2;
-        // int[] res = new int[n];
-
-        // int i=0;
-        // int j=0;
-        // int k=0;
-
-        // while(i < nums1.length && j < nums2.length){
-            // if(nums1[i]<nums2[j]){
-            //     res[k++] = nums1[i++];
-            // }else{
-            //     res[k++] = nums2[j++];
-            // }
-           
-        // }
-        // while (i < n1) {
-        //     res[k++] = nums1[i++];
-        // }
-        // while (j < n2) {
-        //     res[k++] = nums2[j++];
-        // }
-        // if( n % 2 == 0){
-        //     return (double) (res[n / 2 - 1] + res[n / 2]) / 2.0;
-        // }else{
-        //     return (double)res[n/2];
-        // }
-
-
-
-        // BETTER APPROACH
-        // TC - O(n1+n2) SC - O(1)
+        //TC - O(n1 +n2) SC - O(n1+n2)
 
         int n1 = nums1.length;
         int n2 = nums2.length;
         int n = n1 + n2;
-        int ind2 = (n1+n2)/2;
-        int ind1 =( (n1+n2)/2) -1;
-        int count =0;
-        int ind1element = -1;
-        int ind2element = -1;
+        int[] res = new int[n];
+
         int i=0;
         int j=0;
+        int k=0;
 
-        while(i < n1 && j < n2){
+        while(i < nums1.length && j < nums2.length){
             if(nums1[i]<nums2[j]){
-                if (count == ind1) ind1element = nums1[i];
-                if (count == ind2) ind2element = nums1[i];
-                count++;
-                i++;
+                res[k++] = nums1[i++];
             }else{
-                if (count == ind1) ind1element = nums2[j];
-                if (count == ind2) ind2element = nums2[j];
-                count++;
-                j++;
-            } 
+                res[k++] = nums2[j++];
+            }
+           
         }
         while (i < n1) {
-            if (count == ind1) ind1element = nums1[i];
-            if (count == ind2) ind2element = nums1[i];
-            count++;
-            i++;
+            res[k++] = nums1[i++];
         }
         while (j < n2) {
-            if (count == ind1) ind1element = nums2[j];
-            if (count == ind2) ind2element = nums2[j];
-            count++;
-            j++;
+            res[k++] = nums2[j++];
         }
-        if (n % 2 == 1) {
-            return (double)ind2element;
+        if( n % 2 == 0){
+            return (double) (res[n / 2 - 1] + res[n / 2]) / 2.0;
+        }else{
+            return (double)res[n/2];
         }
-
-        return (double)((double)(ind1element + ind2element)) / 2.0;
-
-
-
-
-        // OPTIMAL APPROACH
-        // TC - O(log(n1,n2)) SC - O(1)
-
 
     }
 }
