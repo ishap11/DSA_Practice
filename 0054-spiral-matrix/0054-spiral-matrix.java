a5b605688b2e9be923882171dac2300775c53b0b
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-10-01 05:40:36 +0530

    Time: 0 ms (100%), Space: 41.2 MB (98.97%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -2,8 +2,11 @@ class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> ans = new ArrayList<>();
 
-        int n = matrix.length;
-        int m = matrix[0].length;
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int tne = m*n;
+        int count  = 0;
 
         int minr = 0;
         int maxr = matrix.length - 1;
@@ -11,35 +14,32 @@ class Solution {
         int minc = 0;
         int maxc = matrix[0].length - 1;
 
-        int tne = n * m;
-        int count = 0;
-
         while(count < tne){
-            for(int j = minc ; j<= maxc && count < tne; j++){
-                ans.add(matrix[minr][j]);
+            for(int i = minc ; i <= maxc  && count < tne; i++){
+                ans.add(matrix[minr][i]);
                 count++;
             }
             minr++;
 
-            for(int i = minr ; i <= maxr && count < tne; i++){
+            for(int i  = minr ; i <= maxr  && count < tne; i++){
                 ans.add(matrix[i][maxc]);
                 count++;
             }
             maxc--;
 
-            for(int j = maxc ; j>= minc && count < tne; j--){
-                ans.add(matrix[maxr][j]);
+            for(int i = maxc ; i>=minc && count < tne; i--){
+                ans.add(matrix[maxr][i]);
                 count++;
             }
             maxr--;
 
-            for(int i = maxr ; i>= minr && count < tne; i--){
+            for(int i = maxr ; i>=minr && count < tne ; i--){
                 ans.add(matrix[i][minc]);
                 count++;
             }
             minc++;
         }
-
         return ans;
     }
+
 }
