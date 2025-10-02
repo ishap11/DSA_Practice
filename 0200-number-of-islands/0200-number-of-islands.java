4abada8966f3ffcc2191123fc09d84c7d2c76b92
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-10-02 06:29:09 +0530

    Time: 3 ms (84.54%), Space: 48.9 MB (93.64%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,14 +1,16 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int count = 0;
-        int n = grid.length;
+        int count =  0;
+
+        int n = grid.length ;
         int m = grid[0].length;
+
         int[][] visited = new int[n][m];
 
-        for(int i=0; i< n  ;i++){
-            for(int j=0; j< m ; j++){
+        for(int i = 0; i< n ; i++){
+            for(int j = 0 ; j< m ; j++){
                 if(grid[i][j] == '1' && visited[i][j] == 0){
-                    drawTreeForComp(grid , i, j , visited);
+                    findIsland(grid , i , j , visited);
                     count++;
                 }
             }
@@ -16,16 +18,17 @@ class Solution {
         return count;
     }
 
-
-    public static void drawTreeForComp(char[][] grid , int i , int j, int[][] visited){
-        if(i < 0 || j < 0 || i >= grid.length || j >= grid[i].length || grid[i][j] == '0' || visited[i][j] == 1){
-        return;
-    }
+    private void findIsland(char[][] grid , int i , int j , int[][] visited){
+        if(i<0 || j<0|| i>= grid.length || j>= grid[0].length || grid[i][j] == '0' || visited[i][j] == 1){
+            return;
+        }
 
         visited[i][j] = 1;
-        drawTreeForComp(grid , i-1 , j , visited); //north
-        drawTreeForComp(grid , i , j+1, visited); //east
-        drawTreeForComp(grid , i+1 , j , visited); //south
-        drawTreeForComp(grid , i , j-1 , visited); //west
+
+        findIsland(grid , i-1 , j , visited);
+        findIsland(grid , i+1 , j , visited);
+        findIsland(grid , i , j-1 , visited);
+        findIsland(grid , i , j+1 , visited);
+
     }
 }
