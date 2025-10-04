4c668187a316cbb75b7dd244e19465b91463338b
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-10-04 05:46:15 +0530

    Time: 0 ms (100%), Space: 45.2 MB (34.17%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,25 +1,25 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        if(image[sr][sc] == color) {
+        if(color == image[sr][sc]){
             return image;
         }
 
-        recurs(image, sr, sc, color, image[sr][sc]);
+        recurs(image , sr , sc , color , image[sr][sc]);
         return image;
     }
-    public void recurs(int[][] image, int sr, int sc, int color, int cur) {
-        if(sr < 0 || sr >= image.length || sc < 0 || sc >= image[0].length){
+
+    public void recurs(int[][] image , int sr , int sc , int color , int curr){
+        if(sr < 0 || sc < 0 || sr >= image.length || sc >= image[0].length) {
             return;
-        } 
-       
-        if(cur != image[sr][sc]){
+        }
+        if(curr != image[sr][sc]){
             return;
         }
 
         image[sr][sc] = color;
-        recurs(image, sr-1, sc, color, cur);
-        recurs(image, sr+1, sc, color, cur);
-        recurs(image, sr, sc-1, color, cur);
-        recurs(image, sr, sc+1, color, cur);
+        recurs(image , sr + 1 , sc , color , curr);
+        recurs(image , sr - 1 , sc , color , curr);
+        recurs(image , sr  , sc + 1, color , curr);
+        recurs(image , sr , sc - 1, color , curr);
     }
 }
