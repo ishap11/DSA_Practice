83b359eac4d091b843b0a698b6d160ce48fa6594
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-10-05 07:45:30 +0530

    Time: 0 ms (100%), Space: 51.6 MB (15.82%) - LeetHub

--- 0975-range-sum-of-bst/0975-range-sum-of-bst.java
+++ 0975-range-sum-of-bst/0975-range-sum-of-bst.java
@@ -15,30 +15,21 @@
  */
 class Solution {
     public int rangeSumBST(TreeNode root, int low, int high) {
-         if (root == null) {
-            return 0;
+        if(root == null) return 0;
+        int sum = 0;
+
+        if(low <= root.val && root.val <= high){
+            sum += root.val;
+
+            sum += rangeSumBST(root.left , low , high);
+            sum += rangeSumBST(root.right , low , high);
+        }
+        else if(root.val <= high){
+            sum += rangeSumBST(root.right , low , high);
+        }else{
+            sum += rangeSumBST(root.left , low , high);
         }
 
-        int sum = 0;
-        if (root.val >= low && root.val <= high) {
-            sum += root.val;
-            // Traverse both subtrees, as they might contain other nodes in the range.
-            sum += rangeSumBST(root.left, low, high);
-            sum += rangeSumBST(root.right, low, high);
-        } 
-        // If the current node's value is less than the low bound, 
-        // all values in its left subtree will also be too small.
-        // We only need to check the right subtree.
-        else if (root.val < low) {
-            sum += rangeSumBST(root.right, low, high);
-        }
-        // If the current node's value is greater than the high bound,
-        // all values in its right subtree will also be too large.
-        // We only need to check the left subtree.
-        else { // root.val > high
-            sum += rangeSumBST(root.left, low, high);
-        }
-        
         return sum;
     }
 }
