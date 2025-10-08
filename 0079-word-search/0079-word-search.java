26edce31b3943a4d84d89f1cfad19879996fd188
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-10-08 07:21:11 +0530

    Time: 140 ms (58.65%), Space: 41.2 MB (98.35%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -2,13 +2,13 @@ class Solution {
     public boolean exist(char[][] board, String word) {
         int n = board.length;
         int m = board[0].length;
-
+        
         boolean[][] visited = new boolean[n][m];
 
-        for(int i = 0 ; i< n ; i++){
-            for(int j = 0 ; j<m ; j++){
-                if (board[i][j] == word.charAt(0)) {
-                    if (isPathExist(board, word, visited, i, j, 0)) {
+        for(int i = 0 ; i < n ;i++){
+            for(int j = 0 ; j< m ; j++){
+                if(board[i][j] == word.charAt(0)){
+                    if(isPathExist(board , word , visited , i , j , 0)){
                         return true;
                     }
                 }
@@ -17,13 +17,14 @@ class Solution {
         return false;
     }
 
-    private boolean isPathExist(char[][] board , String word , boolean[][] visited , int row , int col , int idx){
-        if(idx == word.length()) return true;
+    public boolean isPathExist(char[][] board , String word , boolean[][] visited , int row , int col , int idx){
 
+        if(idx == word.length()) return true;
 
         if (row < 0 || col < 0 || row >= board.length || col >= board[0].length || visited[row][col] || board[row][col] != word.charAt(idx)) {
             return false;
         }
+
         visited[row][col] = true;
 
         if (isPathExist(board, word, visited, row - 1, col, idx + 1) ||  // top
