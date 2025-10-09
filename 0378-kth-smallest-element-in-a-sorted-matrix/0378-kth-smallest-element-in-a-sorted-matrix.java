eefd24f35b34d436c27a766e5a8eadd74e4d3d78
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-10-09 06:18:22 +0530

    Time: 10 ms (66.5%), Space: 46.8 MB (99.83%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,7 +1,10 @@
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-        int n = matrix.length;
+        // FIrst APPROACH
+// Time complexity : N * log(N);  //N == n^2
+// space complexity: O(N)
 
+        int n= matrix.length;
         int[] arr = new int[n*n];
         int index = 0;
         for(int i=0; i<n ; i++){
@@ -12,5 +15,45 @@ class Solution {
         Arrays.sort(arr);
         
         return arr[k - 1];
+
+        // Efficient approaxch -  binary search
+        //  Appraoch 3 (Best Approach) /* By noticing the Sorted array, Binary Search 
+// Comes into play but , the implementation is a little different in 2d matrix
+// case so if it is known beforehand then only it strikes*/
+
+// Time Complexity:O(N) //N== n^2
+// Auxiliary Space Complexity: O(1)
+
+// class Solution {
+//     public int kthSmallest(int[][] matrix, int k) {
+//         int rows = matrix.length, cols = matrix[0].length;
+        
+//         int lo = matrix[0][0], hi = matrix[rows - 1][cols - 1] ;
+//         while(lo <= hi) {
+//             int mid = lo + (hi - lo) / 2;
+//             int count = 0,  maxNum = lo;
+           
+//             for (int r = 0, c = cols - 1; r < rows; r++) {
+//                 while (c >= 0 && matrix[r][c] > mid) c--;   
+                
+//                 if (c >= 0) {
+//                     count += (c + 1); // count of nums <= mid in matrix
+//                     maxNum = Math.max(maxNum, matrix[r][c]); 
+//          // mid might be value not in  matrix, we need to record the actually max num;
+//                 }else{ //it means c < 0
+//                     break;
+//                 } 
+//             }
+            
+//             // adjust search range
+//             if (count == k) return maxNum;
+//             else if (count < k) lo = mid + 1;
+//             else hi = mid - 1;
+//         }
+//         return lo;
     }
+
 }
+        
+//     }
+// }
