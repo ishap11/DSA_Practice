bd05f129795db6c4961aa86400e3c2dfa8a45c1a
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-10-10 08:43:54 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int mpist = 0;
+        int leastsf = prices[0];
+        int[] dpl = new int[prices.length];
+
+        for(int i = 1; i < prices.length ; i++){
+            if(leastsf > prices[i]){
+                leastsf = prices[i];
+            }
+            mpist = prices[i] - leastsf;
+
+            if(mpist > dpl[i-1]){
+                dpl[i] = mpist;
+            }else{
+                dpl[i] = dpl[i-1];
+            }
+        }
+
+        int mpibt = 0;
+        int maxsf = prices[prices.length - 1];
+        int[] dpr = new int[prices.length];
+
+        for(int i = prices.length - 2; i>= 0 ; i--){
+            if(maxsf < prices[i]){
+                maxsf = prices[i];
+            }
+
+            mpibt = maxsf - prices[i];
+
+            if(mpibt > dpl[i+1]){
+                dpr[i] = mpibt;
+            }else{
+                dpr[i] = dpr[i+1];
+            }
+        }
+
+        int op = 0;
+        for(int i = 0 ; i< prices.length ; i++){
+            if(dpl[i] + dpr[i] > op){
+                op = dpl[i] + dpr[i];
+            }
+        }
+
+        return op;
+    }
+}
