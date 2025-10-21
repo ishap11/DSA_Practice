e09988232714fefe87f676d9102c89df2475b372
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-10-21 12:01:18 +0530

    Time: 2 ms (65.7%), Space: 42.4 MB (62.87%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,11 +1,13 @@
 class Solution {
-    public class Pair{
-        int row;
-        int col ; 
+    static class Node{
+        int i ;
+        int j ;
+        int time;
 
-        Pair(int row , int col ){
-            this.row = row;
-            this.col = col;
+        Node(int i , int j , int time){
+            this.i = i;
+            this.j = j;
+            this.time = time;
         }
     }
 
@@ -13,48 +15,56 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
 
-        LinkedList<Pair> queue = new LinkedList<>();
-        int time = 0;
+        Queue<Node> que = new LinkedList<>();
+        boolean[][] visited = new boolean[m][n];
+        int ans = 0;
 
-        for(int i=0; i< m ; i++){
-            for(int j= 0; j< n ; j++){
+        for(int i = 0 ; i< m ; i++){
+            for(int j = 0 ; j< n ;j++){
                 if(grid[i][j] == 2){
-                    queue.addLast(new Pair(i,j));
-                }else if(grid[i][j] == 1){
-                    time++;
+                    que.add(new Node(i , j , 0));
                 }
             }
         }
 
-        if(time == 0){
-            return 0;
+        while(que.size() > 0){
+            Node curr = que.remove();
+            int i = curr.i;
+            int j = curr.j;
+            int time = curr.time;
+
+            ans = Math.max(ans , time);
+
+            if(i-1 >= 0 && !visited[i-1][j] && grid[i-1][j] == 1){
+                que.add(new Node(i-1 , j , time + 1));
+                visited[i-1][j] = true;
+            }
+
+            if(i+1 < m && !visited[i+1][j] && grid[i+1][j] == 1){
+                que.add(new Node(i+1 , j , time + 1));
+                visited[i+1][j] = true;
+            }
+
+
+            if(j-1 >= 0 && !visited[i][j-1] && grid[i][j-1] == 1){
+                que.add(new Node(i , j-1 , time + 1));
+                visited[i][j-1] = true;
+            }
+
+            if(j+1 < n && !visited[i][j+1] && grid[i][j+1] == 1){
+                que.add(new Node(i , j+1 , time + 1));
+                visited[i][j+1] = true;
+            }
         }
-        int[][] direction = {{-1 ,0 }, {0,1} , {1,0} , {0,-1}};
-        int level = -1;
-        // bfs
-        while(!queue.isEmpty()) {
-            int size = queue.size();
-            level++;
 
-            while(size-- > 0){
-                Pair rem = queue.removeFirst();
-                for(int i=0; i< 4 ; i++){
-                    int rdash  = rem.row + direction[i][0];
-                    int cdash = rem.col + direction[i][1];
-
-                    if(rdash >=0 && cdash >= 0 && rdash < m && cdash < n && grid[rdash][cdash] == 1){
-                        queue.addLast(new Pair(rdash, cdash));
-                        grid[rdash][cdash] = 0;
-                        time--;
-                    }
+        for(int i = 0 ; i< m ; i++){
+            for(int j = 0 ; j< n ;j++){
+                if(grid[i][j] == 1 && !visited[i][j]){
+                    return -1;
                 }
             }
         }
 
-        if(time == 0){
-            return level;
-        }else{
-            return -1;
-        }
+        return ans;
     }
 }
