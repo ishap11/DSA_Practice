a48e6d4d6392b0ae809e16d4622197fab433e9ac
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-10-21 12:17:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -15,32 +15,32 @@
  */
 class Solution {
     public class BSTPair{
+        int min;
+        int max;
         boolean isBST;
-        long min;
-        long max;
     }
-
-    public BSTPair isBST_(TreeNode root) {
-        if(root == null) {
-            BSTPair bp =new BSTPair();
+    
+    public BSTPair isValidBST_(TreeNode root){
+        if(root == null){
+            BSTPair bp = new BSTPair();
+            bp.min = Integer.MAX_VALUE;
+            bp.max = Integer.MIN_VALUE;
             bp.isBST = true;
-            bp.min = Long.MAX_VALUE;
-            bp.max = Long.MIN_VALUE;
 
             return bp;
         }
 
-        BSTPair lp = isBST_(root.left) ;
-        BSTPair rp = isBST_(root.right);
+        BSTPair lp = isValidBST_(root.left);
+        BSTPair rp = isValidBST_(root.right);
 
         BSTPair mp = new BSTPair();
         mp.isBST = lp.isBST && rp.isBST && (root.val > lp.max && root.val < rp.min);
         mp.min = Math.min(root.val , Math.min(lp.min , rp.min));
-        mp.max = Math.max(root.val , Math.max(lp.max, rp.max));
+        mp.max = Math.max(root.val , Math.max(lp.max , rp.max));
 
         return mp;
     }
     public boolean isValidBST(TreeNode root) {
-        return isBST_(root).isBST;
+        return isValidBST_(root).isBST;
     }
 }
