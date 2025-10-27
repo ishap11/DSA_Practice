eccd4a0c8a99915d68963d3bb1eba3c1349e7d92
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-10-27 07:35:57 +0530

    Time: 0 ms (100%), Space: 45.5 MB (94.16%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -4,10 +4,10 @@ class Solution {
         int n = matrix[0].length;
 
         boolean[] row = new boolean[m];
-        boolean[] col = new boolean[n];
-        
-        for(int i = 0 ; i< m ; i++){
-            for(int j = 0 ; j<n ; j++){
+        boolean[]col = new boolean[n];
+
+        for(int i = 0 ; i<m ; i++){
+            for(int j = 0 ; j< n ; j++){
                 if(matrix[i][j] == 0){
                     row[i] = true;
                     col[j] = true;
@@ -15,10 +15,10 @@ class Solution {
             }
         }
 
-        for(int i = 0 ; i< m ; i++){
+        for(int i = 0 ; i<m ; i++){
             for(int j = 0 ; j< n ; j++){
                 if(row[i] || col[j]){
-                    matrix[i][j] = 0;
+                   matrix[i][j] = 0 ;
                 }
             }
         }
