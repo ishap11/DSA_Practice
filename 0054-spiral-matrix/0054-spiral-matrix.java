44cfdf7ed327b2bde7e573cff9f7827c8feb0ebb
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-10-27 08:06:49 +0530

    Time: 0 ms (100%), Space: 41.7 MB (27.97%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -5,8 +5,8 @@ class Solution {
         int m = matrix.length;
         int n = matrix[0].length;
 
-        int tne = m*n;
-        int count  = 0;
+        int tne = m * n;
+        int count = 0;
 
         int minr = 0;
         int maxr = matrix.length - 1;
@@ -15,31 +15,31 @@ class Solution {
         int maxc = matrix[0].length - 1;
 
         while(count < tne){
-            for(int i = minc ; i <= maxc  && count < tne; i++){
+            for(int i = minc ; i <= maxc && count < tne ; i++){
                 ans.add(matrix[minr][i]);
                 count++;
             }
             minr++;
 
-            for(int i  = minr ; i <= maxr  && count < tne; i++){
+            for(int i = minr ; i <= maxr && count < tne ; i++){
                 ans.add(matrix[i][maxc]);
                 count++;
             }
             maxc--;
 
-            for(int i = maxc ; i>=minc && count < tne; i--){
+            for(int i = maxc ; i >= minc && count < tne ; i--){
                 ans.add(matrix[maxr][i]);
                 count++;
             }
             maxr--;
 
-            for(int i = maxr ; i>=minr && count < tne ; i--){
+            for(int i = maxr ; i >= minr && count < tne ; i--){
                 ans.add(matrix[i][minc]);
                 count++;
             }
             minc++;
+
         }
         return ans;
     }
-
 }
