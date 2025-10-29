7dc434b41aac810bb345c027da1516e71df6384a
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-10-29 06:21:55 +0530

    Time: 13 ms (83.4%), Space: 47.9 MB (25.54%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,39 +1,63 @@
 class Solution {
-    public class Pair {
-        int x;
-        int y;
-
-        Pair(int x , int y){
-            this.x = x;
-            this.y = y;
+    public class Pair{
+        int i;
+        int j;
+        int distance;
+        
+        Pair(int i , int j , int distance){
+            this.i = i;
+            this.j = j;
+            this.distance = distance;
         }
     }
-    private int[][] dirs = {{0,1} , {0,-1} , {1,0} , {-1, 0}};
+
     public int[][] updateMatrix(int[][] mat) {
-        LinkedList<Pair> queue = new LinkedList<>();
-
-        for(int i=0; i< mat.length ; i++){
-            for(int j= 0; j< mat[0].length ; j++){
-                if(mat[i][j] == 0 ){
-                    queue.addLast(new Pair(i,j));
-                }else{
-                    mat[i][j] = -1;
+        int m = mat.length;
+        int n = mat[0].length;
+        
+        int[][] ans = new int[m][n];
+        Queue<Pair> que = new LinkedList<>();
+        boolean[][] visited = new boolean[m][n];
+        
+        for(int i = 0 ; i< m ; i++){
+            for(int j = 0;  j< n ; j++){
+                if(mat[i][j] == 0){
+                    que.add(new Pair(i, j , 0));
+                    visited[i][j] = true;
                 }
             }
         }
-
-        while(!queue.isEmpty()){
-            Pair  rem = queue.removeFirst();
-            for(int i=0; i<dirs.length ; i++){
-                int rowdash = rem.x + dirs[i][0];
-                int coldash = rem.y  + dirs[i][1];
-
-                if(rowdash >= 0 && coldash >= 0 && rowdash < mat.length && coldash < mat[0].length && mat[rowdash][coldash ] <0 ){
-                    queue.addLast(new Pair(rowdash , coldash));
-                    mat[rowdash][coldash] = mat[rem.x][rem.y] +1;
-                }
+        
+        while(que.size() > 0){
+            Pair curr = que.remove();
+            int i = curr.i;
+            int j = curr.j;
+            int distance = curr.distance;
+            
+             ans[i][j] = distance;
+            
+            // Check 4 directions
+            if(i-1 >= 0 && !visited[i-1][j]){
+                que.add(new Pair(i-1 , j , distance + 1));
+                visited[i-1][j] = true;
+            }
+            
+            if(i+1 < m && !visited[i+1][j]){
+                que.add(new Pair(i+1 , j , distance + 1));
+                visited[i+1][j] = true;
+            }
+            
+            if(j-1 >= 0 && !visited[i][j-1]){
+                que.add(new Pair(i , j-1 , distance + 1));
+                visited[i][j-1] = true;
+            }
+            
+            if(j+1 < n && !visited[i][j+1]){
+                que.add(new Pair(i , j+1 , distance + 1));
+                visited[i][j+1] = true;
             }
         }
-        return mat;
+        
+        return ans;
     }
 }
