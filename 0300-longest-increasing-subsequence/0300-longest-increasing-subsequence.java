749d4182cf9725b952c1ee648cdaa59515e81c80
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-11-01 15:11:55 +0530

    Time: 51 ms (35.42%), Space: 46 MB (24.12%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,12 +1,11 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
+        int omax = 0;
         int[] dp = new int[nums.length];
 
-        int omax = 0;
-
-        for(int i = 0;  i< dp.length ; i++){
+        for(int i = 0 ; i< nums.length ; i++){
             int max = 0;
-            for(int j= 0 ; j<i ; j++){
+            for(int j = 0 ; j<i ; j++){
                 if(nums[j] < nums[i]){
                     if(dp[j] > max){
                         max = dp[j];
@@ -14,12 +13,9 @@ class Solution {
                 }
             }
             dp[i] = max + 1;
-
-            if(dp[i] > omax){
-                omax = dp[i];
-            }
+            omax = Math.max(omax , dp[i]);
         }
+        
         return omax;
-
     }
 }
