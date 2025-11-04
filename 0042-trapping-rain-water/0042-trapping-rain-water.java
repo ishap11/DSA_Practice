0b2ea436500e49d9babbfa8799f136ba27263065
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-11-04 15:32:59 +0530

    Time: 1 ms (58.23%), Space: 47.5 MB (5.01%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,26 +1,24 @@
 class Solution {
     public int trap(int[] height) {
-        int n = height.length;
-        int l=0;
-        int r = n-1;
+        int ans = 0;
+        int l = 0;
+        int h = height.length - 1;
 
         int lmax = 0;
         int rmax = 0;
-        int ans = 0;
 
-        while( l < r){
+        while(l < h){
             lmax = Math.max(lmax , height[l]);
-            rmax = Math.max(rmax , height[r]);
+            rmax = Math.max(rmax , height[h]);
 
             if(lmax < rmax){
                 ans += lmax - height[l];
                 l++;
-            }else {
-                ans += rmax - height[r];
-                r--;
+            }else{
+                ans += rmax - height[h];
+                h--;
             }
         }
         return ans;
-
     }
 }
