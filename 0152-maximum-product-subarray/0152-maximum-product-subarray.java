8f7938bc8bf21bc2775078339aca6605edcb15d8
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-11-04 15:47:01 +0530

    Time: 0 ms (100%), Space: 47.1 MB (6.39%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -3,27 +3,26 @@ class Solution {
         int ans = Integer.MIN_VALUE;
         int cprod = 1;
 
-        for(int i = 0 ; i< nums.length - 1 ; i++){
+        for(int i = 0 ; i < nums.length - 1 ; i++){
             cprod *= nums[i];
 
             ans = Math.max(ans , cprod);
 
-            if(cprod == 0) {
+            if(cprod == 0){
                 cprod = 1;
             }
         }
 
         cprod = 1;
-        for(int i = nums.length - 1 ; i >= 0 ; i--){
+        for(int i = nums.length -1 ; i >= 0 ; i--){
             cprod *= nums[i];
 
             ans = Math.max(ans , cprod);
 
-            if(cprod == 0) {
+            if(cprod == 0){
                 cprod = 1;
             }
         }
-
         return ans;
     }
 }
