e797b2a8e49d55226ca9166f8d14944c92def3d0
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-11-04 17:54:23 +0530

    Time: 19 ms (46.62%), Space: 45.3 MB (34.13%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,22 +1,23 @@
 class Solution {
     public String longestPalindrome(String s) {
-        String res = "";
+        String ans = "";
 
-        for(int ax =  0 ; ax < s.length() ; ax++){
+        for(int ax = 0 ; ax< s.length() ; ax++){
             int orb = 1;
             int len = 1;
 
-            while(ax-orb >= 0 && ax + orb < s.length()) {
-                if(s.charAt(ax - orb) == s.charAt(ax+orb)){
+            while(ax - orb >= 0 && ax + orb < s.length()){
+                if(s.charAt(ax - orb) == s.charAt(ax + orb)){
                     orb++;
-                    len +=2;
+                    len+=2;
                 }else{
                     break;
                 }
             }
-            if(len > res.length()){
-                int si = ax - len/2;
-                res = s.substring(si , si + len);
+
+            if(len > ans.length()){
+                int si = ax - len / 2;
+                ans = s.substring(si , si + len);
             }
         }
 
@@ -24,22 +25,21 @@ class Solution {
             int orb = 1;
             int len = 0;
 
-
             while(ax - orb + 1 >= 0 && ax + orb < s.length()){
                 if(s.charAt(ax - orb + 1) == s.charAt(ax + orb)){
                     orb++;
                     len += 2;
-                }else{
+                }
+                else{
                     break;
                 }
             }
 
-            if(len > res.length()) {
+            if(len > ans.length()){
                 int si = ax - len/2 + 1;
-                res = s.substring(si , si+len);
+                ans = s.substring(si , si + len);
             }
         }
-
-        return res;
+        return ans;
     }
 }
