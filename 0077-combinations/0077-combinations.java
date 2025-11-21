08cc6c03cdb8d36f289b98d5a32b91ae1e2cfca5
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-11-21 07:27:36 +0530

    Time: 18 ms (78.75%), Space: 97.9 MB (25.04%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,21 +1,20 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> ans = new ArrayList<>();
-
-        backTrack(1 , n , k , new ArrayList<>() , ans);
-
+        
+        generateCombinations(1 , n  , k , new ArrayList<>() , ans);
         return ans;
     }
 
-    public void backTrack(int st , int n , int k , List<Integer>res , List<List<Integer>> ans) {
+    public void generateCombinations(int st , int n , int k , List<Integer> res , List<List<Integer>> ans){
         if(res.size() == k){
             ans.add(new ArrayList<>(res));
             return;
         }
 
-        for(int i = st ; i <= n ; i++){
+        for(int i = st ; i <= n ;i++){
             res.add(i);
-            backTrack(i+1 , n , k , res , ans);
+            generateCombinations(i+1 , n , k , res , ans);
             res.remove(res.size() - 1);
         }
     }
