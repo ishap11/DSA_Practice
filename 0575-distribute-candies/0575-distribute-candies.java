01d2f64fc34188492943928ebde6cc30cafc2045
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-11-23 07:38:49 +0530

    Time: 35 ms (15.72%), Space: 48.5 MB (20.95%) - LeetHub

---
+++ 0575-distribute-candies/0575-distribute-candies.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int distributeCandies(int[] candyType) {
+        
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0; i< candyType.length  ;i++){
+            if(!set.contains(candyType[i])){
+                set.add(candyType[i]);
+            }
+        }
+        int n=candyType.length/2;
+        if(n>=set.size()){
+            return set.size();
+        }
+        else{
+            return n;
+        }
+    }
+}
