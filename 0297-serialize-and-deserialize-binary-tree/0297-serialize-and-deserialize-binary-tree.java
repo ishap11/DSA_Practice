05eef1048282836e455d3840ba2ef62f0553df78
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-11-25 07:45:09 +0530

    Time: 7 ms (95.15%), Space: 49 MB (5.14%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
@@ -8,47 +8,52 @@
  * }
  */
 public class Codec {
-    private void serialize_(TreeNode root , StringBuilder sb) {
-        if(root == null) {
-            sb.append("null,");
-            return;
-        }
-
-        sb.append(root.val).append(",");
-        serialize_(root.left , sb);
-        serialize_(root.right , sb);
-
-    }
 
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
         StringBuilder sb = new StringBuilder();
-        serialize_(root, sb);
 
+        serialize_(root , sb);
         return sb.toString();
     }
 
-    static int idx = 0;
-    private TreeNode deserialize_(String[] arr) {
-
-        if(idx >= arr.length || arr[idx].equals("null")) {
-            idx++;
-            return null;
+    public void serialize_(TreeNode root , StringBuilder sb) {
+        if(root == null){
+            sb.append("null,");
+            return;
         }
-        TreeNode node = new TreeNode(Integer.parseInt(arr[idx++]));
-        node.left = deserialize_(arr);
-        node.right = deserialize_(arr);
+        sb.append(root.val).append(",");
+        serialize_(root.left, sb);
+        serialize_(root.right , sb);
 
-        return node;
     }
+
+    static int idx  = 0;
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        if(data == null || data.isEmpty()) return null;
+        if(data == null || data.isEmpty()){
+            return null;
+        }
 
         idx = 0;
         String[] arr = data.split(",");
         return deserialize_(arr);
     }
+
+    public TreeNode deserialize_(String[] arr){
+        if(idx >= arr.length|| arr[idx].equals("null")){
+            idx++;
+            return null;
+        }
+
+        TreeNode node = new TreeNode(Integer.parseInt(arr[idx++]));
+        node.left = deserialize_(arr);
+        node.right = deserialize_(arr);
+
+
+        return node;
+    }
+
 }
 
 // Your Codec object will be instantiated and called as such:
