897ddf73aac3020de09f50654cbd21cc566467e3
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-12-06 16:52:14 +0530

    Time: 5 ms (96.85%), Space: 42.7 MB (59.21%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,22 +1,24 @@
 class Solution {
     public int countSubstrings(String s) {
         int count = 0;
-        int n = s.length();
 
-        for(int center = 0 ; center < n ; center++){
+        for(int center = 0 ; center < s.length() ; center++){
             count += expandAroundCenter(s , center , center);
             count += expandAroundCenter(s , center , center + 1);
         }
+
         return count;
     }
 
     public int expandAroundCenter(String s , int left , int right){
         int count = 0;
+
         while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
             count++;
             left--;
             right++;
         }
+
         return count;
     }
 }
