6878d93fe7b1d250b4acf3634f28c87e71f42fcc
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-12-06 16:20:47 +0530

    Time: 4 ms (51.99%), Space: 44.5 MB (48.98%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -2,26 +2,29 @@ class Solution {
     public boolean isPalindrome(String s) {
         StringBuilder sb = new StringBuilder();
 
-        for(char ch : s.toCharArray()) {
-            if(Character.isLetterOrDigit(ch)) {
+        for(int i = 0 ; i < s.length() ; i++){
+            char ch = s.charAt(i);
+
+            if(Character.isLetterOrDigit(ch)){
                 sb.append(Character.toLowerCase(ch));
             }
         }
 
         String str = sb.toString();
 
-        int i=0;
-        int j= str.length() -1 ;
+        int i = 0;
+        int j = str.length() - 1;
 
-        while(i<=j) {
-            if(str.charAt(i) == str.charAt(j)) {
+        while(i <= j){
+            if(str.charAt(i) == str.charAt(j)){
                 i++;
                 j--;
-            }else {
+            }
+            else{
                 return false;
             }
         }
-
         return true;
+        
     }
 }
