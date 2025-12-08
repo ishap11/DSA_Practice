bb86528eeca031cfc369e78ab907a54a39d012f1
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-12-08 09:44:35 +0530

    Time: 14 ms (76.72%), Space: 49.6 MB (29.31%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.java
+++ 0085-maximal-rectangle/0085-maximal-rectangle.java
@@ -3,18 +3,18 @@ class Solution {
         int n = matrix.length;
         int m = matrix[0].length;
         if(n == 0) return 0;
-
-        int maxArea = 0;
         int[] heights = new int[m];
+        int maxArea = 0;
 
         for(int i = 0 ; i < n ; i++){
             for(int j = 0 ; j < m ; j++){
-                if(matrix[i][j] == '0'){
+                if(matrix[i][j] =='0'){
                     heights[j] = 0;
                 }else{
                     heights[j] += 1;
                 }
             }
+
             maxArea = Math.max(maxArea , largestRectangle(heights));
         }
         return maxArea;
@@ -25,13 +25,16 @@ class Solution {
         int maxArea = 0;
         Stack<Integer> st = new Stack<>();
 
-        for(int i = 0 ;i <= n ; i++){
+        for(int i =0 ; i <= n ;i++){
             int h = (i == n) ? 0 : heights[i];
-        
+
             while(!st.isEmpty() && h < heights[st.peek()]){
                 int height = heights[st.pop()];
                 int width = st.isEmpty() ? i : i - st.peek() - 1;
-                maxArea = Math.max(maxArea , height * width);
+
+                int area = height * width;
+
+                maxArea = Math.max(maxArea , area);
             }
             st.push(i);
         }
