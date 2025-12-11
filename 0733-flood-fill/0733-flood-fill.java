82235077ad414a943e859d9e6676efdf5031e1fc
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-12-11 09:29:12 +0530

    Time: 0 ms (100%), Space: 46.7 MB (50.25%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -9,19 +9,15 @@ class Solution {
     }
 
     public void recurs(int[][] image , int sr , int sc , int color , int curr){
-        if(sr < 0 || sc < 0 || sr >= image.length || sc >= image[0].length ){
-            return;
-        }
-        
-        if(image[sr][sc] != curr){
-            return;
+        if(sr < 0 || sc < 0 || sr >= image.length || sc >= image[0].length || image[sr][sc] != curr){
+            return ;
         }
 
         image[sr][sc] = color;
 
         recurs(image , sr - 1 , sc , color , curr);
-        recurs(image , sr + 1 , sc , color , curr);
         recurs(image , sr , sc - 1 , color , curr);
+        recurs(image , sr + 1 , sc , color , curr);
         recurs(image , sr , sc + 1 , color , curr);
     }
 }
