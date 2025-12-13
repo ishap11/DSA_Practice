bdf9cd10c8d17201841830bc9ff3f415bd6e5725
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-12-13 09:05:55 +0530

    Time: 19 ms (45.61%), Space: 45.2 MB (44.49%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -2,21 +2,21 @@ class Solution {
     public String longestPalindrome(String s) {
         String ans = "";
 
-        for(int ax = 0 ; ax< s.length() ; ax++){
+        for(int ax = 0 ; ax < s.length() ; ax++){
             int orb = 1;
             int len = 1;
 
             while(ax - orb >= 0 && ax + orb < s.length()){
                 if(s.charAt(ax - orb) == s.charAt(ax + orb)){
                     orb++;
-                    len+=2;
+                    len += 2;
                 }else{
                     break;
                 }
             }
 
             if(len > ans.length()){
-                int si = ax - len / 2;
+                int si = ax - len/2;
                 ans = s.substring(si , si + len);
             }
         }
@@ -29,15 +29,14 @@ class Solution {
                 if(s.charAt(ax - orb + 1) == s.charAt(ax + orb)){
                     orb++;
                     len += 2;
-                }
-                else{
+                }else{
                     break;
                 }
             }
 
             if(len > ans.length()){
                 int si = ax - len/2 + 1;
-                ans = s.substring(si , si + len);
+                ans = s.substring(si ,si + len);
             }
         }
         return ans;
