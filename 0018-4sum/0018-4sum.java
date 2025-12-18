f0e95faf8eae08009fa959abf03c460393d55238
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-12-18 09:58:45 +0530

    Time: 19 ms (67.47%), Space: 46.1 MB (16.44%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -3,21 +3,21 @@ class Solution {
         int n = nums.length;
         List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(nums);
-        
+
         for(int i = 0 ; i < n - 3 ; i++){
-            if(i>0 && nums[i] == nums[i-1]) continue;
+            if(i > 0 && nums[i] == nums[i-1]) continue;
 
             for(int j = i + 1 ; j < n - 2 ; j++){
-                if(j > i + 1 && nums[j] == nums[j-1]) continue;
+                if(j > i+1 && nums[j] == nums[j-1]) continue;
 
                 int left = j + 1;
                 int right = n - 1;
 
                 while(left < right){
-                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
+                    long sum = (long)nums[i] + nums[j] + nums[left] + nums[right];
 
                     if(sum == target){
-                        ans.add(new ArrayList<>(Arrays.asList(nums[i] , nums[j] , nums[left] , nums[right])));
+                        ans.add(Arrays.asList(nums[i] , nums[j] , nums[left] , nums[right]));
                         left++;
                         right--;
 
