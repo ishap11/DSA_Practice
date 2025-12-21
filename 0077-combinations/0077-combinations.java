a67f0adf65e984f9993422622cb676c61b6045b1
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-12-21 09:28:15 +0530

    Time: 18 ms (79.83%), Space: 98.4 MB (6.66%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -2,11 +2,11 @@ class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> ans = new ArrayList<>();
 
-        generateCombinations(1 , n , k , new ArrayList<>()  , ans);
+        generate(1 , n , k , new ArrayList<>() , ans);
         return ans;
     }
+    public void generate(int st, int n , int k , List<Integer> res ,  List<List<Integer>> ans){
 
-    public void generateCombinations(int st , int n , int k , List<Integer> res , List<List<Integer>> ans){
         if(res.size() == k){
             ans.add(new ArrayList<>(res));
             return;
@@ -14,7 +14,7 @@ class Solution {
 
         for(int i = st ; i <= n ; i++){
             res.add(i);
-            generateCombinations(i+1 , n , k , res , ans);
+            generate(i + 1 , n , k , res , ans);
             res.remove(res.size() - 1);
         }
     }
