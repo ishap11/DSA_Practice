522ec3a6481fc033ed6fab5fe0f05b14d2d18ea0
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2025-12-25 09:55:53 +0530

    Time: 51 ms (5.2%), Space: 53.6 MB (5.89%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,23 +1,21 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<HashMap<Character, Integer> , List<String>> bmap = new HashMap<>();
-        
+        HashMap< HashMap<Character , Integer> , List<String>> bmap = new HashMap<>();
 
         for(String str : strs){
             HashMap<Character , Integer> fmap = new HashMap<>();
 
-            for(int i = 0 ; i< str.length() ; i++){
+            for(int i = 0 ; i < str.length() ; i++){
                 char ch = str.charAt(i);
 
-                fmap.put(ch , fmap.getOrDefault(ch , 0 ) + 1);
+                fmap.put(ch , fmap.getOrDefault(ch , 0) + 1);
             }
 
-            if(bmap.containsKey(fmap) == false) {
-                List<String> list=  new ArrayList<>();
+            if(bmap.containsKey(fmap) == false){
+                List<String> list = new ArrayList<>();
                 list.add(str);
                 bmap.put(fmap , list);
-            }
-            else{
+            }else{
                 List<String> list = bmap.get(fmap);
                 list.add(str);
             }
