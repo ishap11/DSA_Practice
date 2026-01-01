b48a2784bbb4666de87451472028fdbdcc6466ac
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2026-01-01 09:55:27 +0530

    Time: 9 ms (98.59%), Space: 59.2 MB (82.74%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -9,45 +9,8 @@
  * }
  */
 class Solution {
-    public ListNode mid(ListNode head) {
-        if(head == null || head.next == null) {
-            return head;
-        }
-
-        ListNode slow = head;
-        ListNode fast = head;
-
-        while(fast.next != null && fast.next.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        return slow;
-    }
-
-    public ListNode mergeTwoLL(ListNode l1 , ListNode l2) {
-        if(l1 == null || l2 == null) return l1 != null ? l1 : l2;
-        ListNode dummy = new ListNode(-1);
-        ListNode prev = dummy;
-
-        while(l1 != null && l2 != null) {
-            if(l1.val < l2.val){
-                prev.next = l1;
-                l1 = l1.next;
-            }
-            else {
-                prev.next = l2;
-                l2 = l2.next;
-            }
-            prev = prev.next;
-        }
-
-        prev.next = l1 != null ? l1 : l2;
-
-        return dummy.next;
-    }
-
     public ListNode sortList(ListNode head) {
-        if(head == null || head.next == null) {
+        if(head == null || head.next == null){
             return head;
         }
 
@@ -58,6 +21,44 @@ class Solution {
         ListNode l1 = sortList(head);
         ListNode l2 = sortList(nhead);
 
-        return mergeTwoLL(l1 , l2);
+        return mergeTwoSortedLL(l1 , l2);
+    }
+
+    public ListNode mid(ListNode head){
+        if(head == null || head.next == null){
+            return head;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public ListNode mergeTwoSortedLL(ListNode l1 , ListNode l2){
+        if(l1 == null || l2 == null) return l1 != null ? l1 : l2;
+        ListNode dummy = new ListNode(-1);
+        ListNode prev = dummy;
+
+        while(l1 != null && l2 != null){
+            if(l1.val < l2.val){
+                prev.next = l1;
+                l1 = l1.next;
+            }
+            else{
+                prev.next = l2;
+                l2 = l2.next;
+            }
+
+            prev = prev.next;
+        }
+
+        prev.next = l1 != null ? l1 : l2;
+
+        return dummy.next;
     }
 }
