b719644de820dd7dfd80eaa9f0d8c5b268d2b4ad
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2026-01-01 09:04:18 +0530

    Time: 6 ms (97.42%), Space: 47.1 MB (96.79%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -10,12 +10,13 @@
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        
-        if(p.val < root.val && q.val < root.val) {
-            return lowestCommonAncestor(root.left , p, q);
-        } else if(p.val > root.val && q.val > root.val ) {
+        if(p.val < root.val && q.val < root.val){
+            return lowestCommonAncestor(root.left , p , q);
+        }
+        else if(p.val > root.val && q.val > root.val){
             return lowestCommonAncestor(root.right , p , q);
-        } else {
+        }
+        else{
             return root;
         }
     }
