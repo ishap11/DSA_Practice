c44d12e8dae1fd406fdf51aa019a7e970e515b3c
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2026-01-06 09:34:21 +0530

    Time: 0 ms (100%), Space: 44.7 MB (31.44%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,23 +1,24 @@
 class Solution {
     public void nextPermutation(int[] nums) {
         int i = nums.length - 2;
-
-        while(i>= 0 && nums[i] >= nums[i+1]){
+        while(i >= 0 && nums[i] >= nums[i+1]){
             i--;
         }
+
         if(i == -1){
-            int s = 0;
-            int t = nums.length - 1;
-            while(s <= t){
-                int temp = nums[s];
-                nums[s] = nums[t];
-                nums[t] = temp;
-                s++;
-                t--;
+            int st = 0;
+            int et = nums.length - 1;
+            
+            while(st <= et){
+                int temp = nums[st];
+                nums[st] = nums[et];
+                nums[et] = temp;
+                st++;
+                et--;
             }
             return;
         }
-
+        
         int k = nums.length - 1;
         while(nums[i] >= nums[k]){
             k--;
@@ -26,15 +27,15 @@ class Solution {
         nums[i] = nums[k];
         nums[k] = temp;
 
-        int st = i+1;
-        int et = nums.length - 1;
+        int strt = i + 1;
+        int end = nums.length - 1;
 
-        while(st <= et){
-            int tempp = nums[st];
-            nums[st] = nums[et];
-            nums[et] = tempp;
-            st++;
-            et--;
+        while(strt <= end){
+            int tempp= nums[strt];
+            nums[strt] = nums[end];
+            nums[end] = tempp;
+            strt++;
+            end--;
         }
     }
 }
