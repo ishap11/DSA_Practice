a66e4d3cf086929de855656cf197e784f70e2010
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2026-01-07 09:20:26 +0530

    Time: 0 ms (100%), Space: 46.9 MB (15.75%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,9 +11,8 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if(head== null || head.next == null) {
-            return false;
-        }
+        if(head == null || head.next == null) return false;
+
         ListNode slow = head;
         ListNode fast = head;
 
@@ -21,8 +20,7 @@ public class Solution {
             slow = slow.next;
             fast = fast.next.next;
 
-
-            if(slow == fast) {
+            if(slow == fast){
                 return true;
             }
         }
