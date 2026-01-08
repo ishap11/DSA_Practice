1625fc4f949972a32af77a831823b4151e352787
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2026-01-08 15:43:30 +0530

    Time: 1 ms (64.65%), Space: 43.4 MB (19.4%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,56 +1,18 @@
 class Solution {
-    public class Node {
-        Node[] childs = new Node[26];
-        boolean isEnd = false;
-    }
-
-    public void insert(Node curr , String s) {
-        for(int i=0 ; i< s.length() ; i++) {
-            char ch = s.charAt(i);
-
-            if(curr.childs[ch - 'a'] == null) {
-                curr.childs[ch - 'a'] = new Node();
-            }
-            curr = curr.childs[ch - 'a'];
-        }
-        curr.isEnd = true;
-    }
-
     public String longestCommonPrefix(String[] strs) {
-        if(strs.length == 0) {
-            return "";
-        }
-        
-        Node root = new Node();
+        if (strs.length == 0) return "";
 
-        for(String s : strs) {
-            insert(root , s);
-        }
+        String first = strs[0];
 
-        StringBuilder sb = new StringBuilder();
-        Node curr = root;
-       
-        while (true) {
-            int count = 0;
-            int nextIdx = -1;
+        for(int i = 0 ; i < first.length() ; i++){
+            char ch = first.charAt(i);
 
-            // Check how many children this node has
-            for (int i = 0; i < 26; i++) {
-                if (curr.childs[i] != null) {
-                    count++;
-                    nextIdx = i;
+            for(int j = 1 ; j < strs.length; j++){
+                if (i >= strs[j].length() || strs[j].charAt(i) != ch) { 
+                    return first.substring(0, i); 
                 }
             }
-
-            // ✅ Stop if there is more than one branch or end of word
-            if (count == 1 && !curr.isEnd) {
-                sb.append((char) (nextIdx + 'a'));
-                curr = curr.childs[nextIdx];
-            } else {
-                break;
-            }
         }
-
-        return sb.toString();
+        return first;
     }
 }
