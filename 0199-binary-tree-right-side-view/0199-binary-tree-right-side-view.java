f2d39fc63caa5b4e002f1031f5ea855bad842d34
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2026-01-15 09:39:58 +0530

    Time: 1 ms (73.16%), Space: 43.3 MB (96.57%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -18,27 +18,25 @@ class Solution {
         List<Integer> ans = new ArrayList<>();
 
         if(root == null) return ans;
-
         Queue<TreeNode> que = new LinkedList<>();
         que.add(root);
-
         Integer rightMostVal = null;
 
-        while(!que.isEmpty()) {
+        while(que.size() > 0){
             int size = que.size();
 
-            for(int i=0 ; i< size ; i++) {
-                TreeNode node = que.poll();
+            for(int i = 0 ; i < size ; i++){
+                TreeNode node = que.remove();
                 rightMostVal = node.val;
 
-                if(node.left != null) {
+                if(node.left != null){
                     que.add(node.left);
                 }
-                if(node.right != null) {
+
+                if(node.right != null){
                     que.add(node.right);
                 }
             }
-
             ans.add(rightMostVal);
         }
 
