04eaa123abc0d2f464eca5da19c8db4fb6f77c44
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2026-01-18 15:44:55 +0530

    Time: 404 ms (54.66%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,7 +1,6 @@
 # Write your MySQL query statement below
-SELECT MAX(num) AS num FROM MYNumbers
-WHERE num IN (
-    SELECT num FROM MyNumbers
-    GROUP BY num
-    HAVING COUNT(num) = 1
-);
+select max(num) as num from MyNumbers
+where num In (
+    select num from MyNumbers group by num
+    having count(num) = 1
+)
