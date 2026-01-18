affed3057572b8e09c5b94594d11fac62ea5c6b7
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2026-01-18 10:38:23 +0530

    Time: 3 ms (30.04%), Space: 45.7 MB (79.46%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -14,26 +14,25 @@
  * }
  */
 class Solution {
-    public TreeNode buildTree_(int[] preorder, int psi , int pei , int[] inorder , int isi , int iei) {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        int n = preorder.length;
+        return buildTree_(preorder , 0 , n - 1 , inorder , 0 , n - 1);
+    }
 
+    public TreeNode buildTree_(int[] preorder , int psi , int pei , int[] inorder , int isi , int iei){
         if(isi > iei) return null;
 
-        int idx = isi;
-        while(inorder[idx] != preorder[psi]) {
+        int idx = 0;
+        while(inorder[idx] != preorder[psi]){
             idx++;
         }
-        int colse = idx - isi;
 
+        int colse = idx - isi;
 
         TreeNode root = new TreeNode(preorder[psi]);
         root.left = buildTree_(preorder , psi + 1 , psi + colse , inorder , isi , idx - 1);
-        root.right = buildTree_(preorder , psi + colse + 1 , pei , inorder,  idx + 1 , iei);
+        root.right = buildTree_(preorder , psi + colse + 1 , pei , inorder , idx + 1 , iei);
 
-        return root;
-    }
-    
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
-        int n = preorder.length;
-        return buildTree_(preorder , 0 , n-1 , inorder , 0 , n-1);
+        return root; 
     }
 }
