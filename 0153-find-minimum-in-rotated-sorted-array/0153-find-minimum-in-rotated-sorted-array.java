06e8735e0cad69d9bd3e3466c5dedae56e5c06f8
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2026-01-21 10:31:50 +0530

    Time: 0 ms (100%), Space: 43.9 MB (44.13%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -3,15 +3,17 @@ class Solution {
         int lo = 0;
         int hi = nums.length - 1;
 
-        while(lo < hi){
-            int mid = ( lo + hi)/2;
+        while( lo < hi){
+            int mid = (lo + hi) / 2;
 
             if(nums[mid] > nums[hi]){
-                lo = mid + 1;
-            } else {
+                lo = mid  + 1;
+            }else{
                 hi = mid;
             }
+            
         }
+
         return nums[lo];
     }
 }
