8355574913783423672bc342a828f1b41da22bf0
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2026-01-22 10:31:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -5,23 +5,20 @@ class Solution {
         for(int i = 0 ; i < s.length() ; i++){
             char ch = s.charAt(i);
 
-            if(ch == '(' || ch == '{' || ch == '[' ){
+            if(ch == '(' || ch == '{' || ch =='['){
                 st.push(ch);
-            }
+            } 
+
             else{
-                if(st.isEmpty()) return false;
+                if((st.peek() == '(' && ch == ')') || (st.peek() == '{' && ch == '}') || (st.peek() == '[' && ch == ']')){
+                    st.pop();
+                }
                 else{
-                    if(( st.peek() == '(' && ch == ')')  
-                       || ( st.peek() == '{' && ch == '}') 
-                       || (st.peek() == '[' && ch == ']')) {
-                            st.pop();
-                    }
-                    else{
-                        return false;
-                    } 
+                    return false;
                 }
             }
         }
+
         return st.isEmpty();
     }
 }
