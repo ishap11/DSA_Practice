fb1e6040dc4d49a644fb249ad2d700a9fa2c9d55
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2026-01-22 09:07:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,27 +1,23 @@
 class Solution {
     public int maxArea(int[] height) {
-        int n = height.length;
         int i = 0;
-        int j = n - 1;
-        int sum = 0;
+        int j = height.length - 1;
+        
+        int max = 0;
+        while(i < j){
+            int hght = Math.max(height[i] , height[j]);
+            int width = height[j] - height[i] + 1;
 
+            int ans = width * hght;
 
-        while(i<j){
-            int width = j-i;
-            int hght = Math.min(height[i] , height[j]);
-
-            int area = width * hght;
-
-            if(area > sum) sum = area;
-
-            if(height[i] < height[j]) {
-                i++;
-            }else{
-                j--;
+            if(ans > max){
+                max = ans;
             }
+
+            if(height[i] < height[j]) i++;
+            else j--;
         }
 
-        return sum;
+        return max;
     }
-
 }
