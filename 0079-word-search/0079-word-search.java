e162efbc0673a09665917796c99f7622999528a0
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2026-01-27 09:37:03 +0530

    Time: 141 ms (53.54%), Space: 43.3 MB (26.75%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -3,38 +3,39 @@ class Solution {
         int m = board.length;
         int n = board[0].length;
 
-        boolean[][] visited = new boolean[m][n];
+        boolean[][] vis = new boolean[m][n];
 
-        for(int i = 0 ; i< m; i++){
-            for(int j = 0 ; j< n; j++){
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
                 if(board[i][j] == word.charAt(0)){
-                    if(isPathExist(board , word , visited , i , j , 0)){
+                    if(find(board , word , i , j , vis , 0)){
                         return true;
                     }
                 }
             }
         }
-
         return false;
     }
 
-    public boolean isPathExist(char[][] board , String word , boolean[][] visited , int row , int col , int idx){
-        if(idx == word.length()) return true;
-
-        if(row < 0 || col < 0 || row >= board.length || col >= board[0].length || visited[row][col] || board[row][col] != word.charAt(idx)){
+    public boolean find(char[][] board, String word , int i , int j , boolean[][] vis , int idx){
+        if (idx == word.length()) {
+            return true;
+        }
+        if(i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] != word.charAt(idx) || vis[i][j] ){
             return false;
         }
 
-        visited[row][col] = true;
+        vis[i][j] = true;
 
-        if (isPathExist(board, word, visited, row - 1, col, idx + 1) ||  // top
-            isPathExist(board, word, visited, row, col - 1, idx + 1) ||  // left
-            isPathExist(board, word, visited, row + 1, col, idx + 1) ||  // down
-            isPathExist(board, word, visited, row, col + 1, idx + 1)) {  // right
-            return true;
-        }
+        if( find(board , word , i - 1 , j , vis , idx + 1) ||
+            find(board , word ,  i + 1 , j ,vis , idx + 1) ||
+            find(board , word ,  i , j - 1 , vis , idx + 1) ||
+            find(board , word ,  i , j + 1 ,vis , idx + 1) ){
+                return true;
+            }
 
-        visited[row][col] = false;
+        vis[i][j] = false;
+
         return false;
     }
 }
