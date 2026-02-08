bf79cdceba4cc45982031f1e17823a73ed56f6be
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2026-02-08 16:14:51 +0530

    Time: 0 ms (100%), Space: 42.3 MB (99.22%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,13 +1,5 @@
 class Solution {
     public int rob(int[] nums) {
-        // wrong
-        // int sum =0;
-        // for(int i=0; i< nums.length ; i+=2) {
-        //     sum+= nums[i];
-        // }
-        // return sum;
-
-
         if (nums.length == 0) return 0;
         if (nums.length == 1) return nums[0];
         
@@ -22,3 +14,32 @@ class Solution {
         return rob2;
     }
 }
+
+/*
+rob1 = 0             rob2 = 0
+[ 1     ,   2      ,  3      , 1] 
+  num
+           num
+                     num
+                               num
+
+temp => max(rob1 = 0 + 1  rob2 = 0) ==> 1
+rob1 = rob2 ===> 0;
+rob2 = temp ===> 1;
+
+temp => max(rob1 = 0 + 2  rob2 = 1) ==> 2
+rob1 = rob2 ===> 1;
+rob2 = temp ===> 2;
+
+temp => max(rob1 = 1 + 3  rob2 = 2) ==> 4
+rob1 = rob2 ===> 2;
+rob2 = temp ===> 4;
+
+temp => max(rob1 = 2 + 1  rob2 = 4) ==> 4
+rob1 = rob2 ===> 4;
+rob2 = temp ===> 4;
+
+
+return rob2 ===> 4 ans
+
+*/
