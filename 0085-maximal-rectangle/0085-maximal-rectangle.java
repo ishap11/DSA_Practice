9ed9d97b2395e6c3f905ec0fcbae55fd2cbb3a4c
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2026-02-18 11:32:06 +0530

    Time: 19 ms (40.05%), Space: 50.7 MB (13.82%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.java
+++ 0085-maximal-rectangle/0085-maximal-rectangle.java
@@ -1,47 +1,44 @@
 class Solution {
     public int maximalRectangle(char[][] matrix) {
-        int n = matrix.length;
-        int m = matrix[0].length;
+        int m = matrix.length;
+        int n = matrix[0].length;
 
-        if(n == 0) return 0;
-        int[] heights = new int[m];
-
+        int[] height = new int[n];
         int maxArea = 0;
 
-        for(int i = 0 ; i < n ;i++){
-            for(int j = 0 ; j  < m  ; j++){
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
                 if(matrix[i][j] == '0'){
-                    heights[j] = 0;
+                    height[j] = 0;
                 }
                 else{
-                    heights[j] += 1;
+                    height[j] += 1;
                 }
             }
 
-            maxArea = Math.max(maxArea , largestRectangle(heights));
+            maxArea = Math.max(maxArea , largestRectangle(height));
         }
 
         return maxArea;
     }
 
-    public int largestRectangle(int[] heights){
-        int n  = heights.length;
+    private int largestRectangle(int[] height){
+        int n = height.length;
         int maxArea = 0;
 
-        Stack<Integer> st = new Stack<>();
+        Stack<Integer> st= new Stack<>();
 
         for(int i = 0 ; i <= n ; i++){
-            int h = (i == n) ? 0 : heights[i];
+            int h = (i == n) ? -1 : height[i];
 
-            while(!st.isEmpty() && h < heights[st.peek()]){
-                int height = heights[st.pop()];
+            while(!st.isEmpty() && h < height[st.peek()]){
+                int hght = height[st.pop()];
                 int width = st.isEmpty() ? i : i - st.peek() - 1;
 
-                int area = width * height;
+                int area = width * hght; 
 
                 maxArea = Math.max(maxArea , area);
             }
-
             st.push(i);
         }
 
