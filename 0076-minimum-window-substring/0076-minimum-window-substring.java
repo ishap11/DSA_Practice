6ec041d85ac6f31cbdbaa87e6babf02c175f1d3d
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2026-02-28 08:19:05 +0530

    Time: 174 ms (6.14%), Space: 48.1 MB (11.35%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,17 +1,19 @@
 class Solution {
     public String minWindow(String s, String t) {
         String ans = "";
+        if(t.length() > s.length()) return "";
 
         HashMap<Character , Integer> map1 = new HashMap<>();
-        for(int i = 0 ; i < t.length() ; i++){
+        for(int i = 0 ;  i < t.length() ; i++){
             char ch = t.charAt(i);
 
-            map1.put(ch , map1.getOrDefault(ch , 0) + 1);
+            map1.put(ch , map1.getOrDefault(ch , 0)  + 1);
         }
 
+
+        HashMap<Character , Integer> map2 = new HashMap<>();
         int i = -1;
         int j = -1;
-        HashMap<Character , Integer> map2 = new HashMap<>();
 
         int mct = 0;
         int dmct = t.length();
@@ -20,29 +22,29 @@ class Solution {
             boolean f1 = false;
             boolean f2 = false;
 
-            while(i < s.length() - 1 && mct != dmct){
+            while(i < s.length() - 1 & mct != dmct){
                 f1 = true;
                 i++;
-                
-                char ch = s.charAt(i);
-                map2.put(ch , map2.getOrDefault(ch , 0) + 1);
 
-                if(map2.getOrDefault(ch , 0) <= map1.getOrDefault(ch , 0)){
+                char ch = s.charAt(i);
+                map2.put(ch , map2.getOrDefault(ch, 0) + 1);
+
+                if(map2.getOrDefault(ch ,0) <= map1.getOrDefault(ch , 0)){
                     mct++;
                 }
             }
 
-            while(j < i && dmct == mct){
+            while(j < i && mct == dmct){
                 f2 = true;
 
-                String pans = s.substring(j + 1 , i + 1);
+                String pans = s.substring(j+ 1 , i + 1);
                 if(ans.isEmpty() || ans.length() > pans.length()){
                     ans = pans;
                 }
 
                 j++;
                 char ch = s.charAt(j);
-                
+
                 if(map2.get(ch) == 1){
                     map2.remove(ch);
                 }
@@ -50,7 +52,7 @@ class Solution {
                     map2.put(ch , map2.get(ch) - 1);
                 }
 
-                if(map2.getOrDefault(ch , 0) < map1.getOrDefault(ch , 0)){
+                if(map2.getOrDefault(ch ,0) < map1.getOrDefault(ch , 0)){
                     mct--;
                 }
             }
