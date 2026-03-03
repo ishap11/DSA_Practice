8d21722c1f8d4c11b40c9e6727b86bcbf76022a6
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2026-03-03 08:30:07 +0530

    Time: 12 ms (11.43%), Space: 48.5 MB (98.4%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,5 +1,3 @@
-import java.util.*;
-
 class Solution {
     static class Pair implements Comparable<Pair> {
         int st;
@@ -20,35 +18,28 @@ class Solution {
     }
     
     public int[][] merge(int[][] intervals) {
-        // Step 1: Convert to Pair array
         Pair[] pairs = new Pair[intervals.length];
         for(int i = 0 ; i < intervals.length ; i++){
             pairs[i] = new Pair(intervals[i][0], intervals[i][1]);
         }
-        
-        // Step 2: Sort by start time (and then end time)
         Arrays.sort(pairs);
         
-        // Step 3: Use stack to merge
         Stack<Pair> st = new Stack<>();
         for(int i = 0 ; i < pairs.length ; i++){
             if(st.isEmpty()){
                 st.push(pairs[i]);
             } else {
                 if(st.peek().et < pairs[i].st){
-                    st.push(pairs[i]); // no overlap
+                    st.push(pairs[i]); 
                 } else {
-                    // merge overlapping intervals
                     st.peek().et = Math.max(st.peek().et, pairs[i].et);
                 }
             }
         }
-        
-        // Step 4: Convert stack to sorted list
+
         ArrayList<Pair> list = new ArrayList<>(st);
         Collections.sort(list, (a, b) -> a.st - b.st);
-        
-        // Step 5: Convert to int[][] for LeetCode output
+    
         int[][] res = new int[list.size()][2];
         for(int i = 0; i < list.size(); i++){
             res[i][0] = list.get(i).st;
