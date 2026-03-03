dd517830badaac3cae8b8cb357f38727f930aad3
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2026-03-03 08:39:51 +0530

    Time: 18 ms (81.86%), Space: 98.2 MB (27.89%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,18 +1,18 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> ans = new ArrayList<>();
-        solve(1 , n ,  k , new ArrayList<>() , ans);
 
+        solve(1 , n , k  , new ArrayList<>() , ans);
         return ans;
     }
 
-    public void solve(int st , int n , int k , List<Integer> res , List<List<Integer>> ans){
+    private void solve(int st , int n , int k , List<Integer> res , List<List<Integer>> ans){
         if(res.size() == k){
             ans.add(new ArrayList<>(res));
             return;
         }
 
-        for(int i = st ; i <= n ; i++){
+        for(int i = st  ; i <= n;  i++){
             res.add(i);
             solve(i + 1 , n , k , res , ans);
             res.remove(res.size() - 1);
