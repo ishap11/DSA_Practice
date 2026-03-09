5f5f2dad282252c1a7aaad0840c2ffe441368326
Isha Prajapati <ishaprajapati2022@vitbhopal.ac.in>
2026-03-09 12:09:21 +0530

    Time: 0 ms (100%), Space: 46.9 MB (96.1%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -3,8 +3,9 @@ class Solution {
         int maxProd = Integer.MIN_VALUE;
         int prod = 1;
 
-        for(int i = 0 ; i < nums.length ; i++){
+        for(int i = 0 ; i < nums.length;  i++){
             prod *= nums[i];
+
             maxProd = Math.max(maxProd , prod);
 
             if(prod == 0){
@@ -14,8 +15,9 @@ class Solution {
 
         prod = 1;
 
-        for(int i = nums.length - 1 ; i >= 0 ; i--){
+        for(int i = nums.length -1 ; i >= 0 ;  i--){
             prod *= nums[i];
+
             maxProd = Math.max(maxProd , prod);
 
             if(prod == 0){
